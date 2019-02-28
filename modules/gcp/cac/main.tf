f92c82aa18ae3307d70bf12113556ffbfc6b728c
Sherman Yin <syin@teradici.com>
2019-02-28 12:12:35 -0800

    Adds CACv2 VM module
    Signed-off-by: Sherman Yin <syin@teradici.com>

---
+++ modules/gcp/cac/main.tf
@@ -0,0 +1,105 @@
+locals {
+    prefix = "${var.prefix != "" ? "${var.prefix}-" : ""}"
+    host_name = "${local.prefix}connector-vm"
+}
+
+resource "google_compute_instance" "cac" {
+    provider = "google"
+    name = "${local.host_name}"
+    machine_type = "${var.machine_type}"
+
+    boot_disk {
+        initialize_params {
+            image = "projects/${var.disk_image_project}/global/images/family/${var.disk_image_family}"
+            type = "pd-ssd"
+            size = "${var.disk_size_gb}"
+        }
+    }
+
+    network_interface {
+        subnetwork = "${var.subnet}"
+        network_ip = "${var.private_ip}"
+        access_config = {}
+    }
+
+    tags = [
+        "${local.prefix}tag-ssh",
+        "${local.prefix}tag-icmp",
+        "${local.prefix}tag-http",
+        "${local.prefix}tag-https",
+    ]
+
+    metadata {
+        startup-script = <<SCRIPT
+            sudo echo "            nameservers:" >> /etc/netplan/50-cloud-init.yaml
+            sudo echo "                search: [${var.domain_name}]" >> /etc/netplan/50-cloud-init.yaml
+            sudo echo "                addresses: [${var.domain_controller_ip}]" >> /etc/netplan/50-cloud-init.yaml
+            sudo netplan apply
+        SCRIPT
+
+        ssh-keys = "${var.cac_admin_user}:${file("${var.cac_admin_ssh_pub_key_file}")}"
+    }
+}
+
+resource "null_resource" "cac-dependencies" {
+    depends_on = ["google_compute_instance.cac"]
+
+    triggers {
+        instance_id = "${google_compute_instance.cac.instance_id}"
+    }
+
+    connection {
+        type = "ssh"
+        user = "${var.cac_admin_user}"
+        private_key = "${file(var.cac_admin_ssh_priv_key_file)}"
+        host = "${google_compute_instance.cac.network_interface.0.access_config.0.nat_ip}"
+        insecure = true
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            # wait for domain controller DNS to be ready
+            # Cannot do this too early.  Otherwise the domain controller reboots and the domain will not be resolved again.
+            "until host ${var.domain_name} > /dev/null; do echo 'Trying to resolve ${var.domain_name}. Retrying in 10 seconds...'; sleep 10; sudo netplan apply; done",
+
+            # download CAC (after DNS is available)
+            "curl -L ${var.cac_installer_url} -o /home/${var.cac_admin_user}/cloud-access-connector.tar.gz",
+            "tar xzvf /home/${var.cac_admin_user}/cloud-access-connector.tar.gz",
+
+            # wait for service account to be added
+            # do this last because it takes a while for new AD user to be added in a new Domain Controller
+            "sudo apt install -y ldap-utils",
+            "until ldapwhoami -H ldap://${var.domain_name} -D ${var.service_account_user}@${var.domain_name} -w ${var.service_account_password} > /dev/null 2>&1; do echo 'Waiting for AD account ${var.service_account_user}@${var.domain_name} to become available. Retrying in 10 seconds...'; sleep 10; sudo netplan apply; done"
+        ]
+    }
+}
+
+resource "null_resource" "install-cac" {
+    depends_on = ["null_resource.cac-dependencies"]
+
+    triggers {
+        instance_id = "${google_compute_instance.cac.instance_id}"
+    }
+
+    connection {
+        type = "ssh"
+        user = "${var.cac_admin_user}"
+        private_key = "${file(var.cac_admin_ssh_priv_key_file)}"
+        host = "${google_compute_instance.cac.network_interface.0.access_config.0.nat_ip}"
+        insecure = true
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "export CAM_BASE_URI=${var.cam_url}",
+
+            # TODO: remove for security.
+            # Save command used for reference
+            "echo \"-E /home/${var.cac_admin_user}/cloud-access-connector install -t ${var.token} --accept-policies --insecure --sa-user ${var.service_account_user} --sa-password \"${var.service_account_password}\" --domain ${var.domain_name} --reg-code ${var.registration_code} ${var.ignore_disk_req ? "--ignore-disk-req" : ""}\" > cmd.txt",
+
+            "sudo -E /home/${var.cac_admin_user}/cloud-access-connector install -t ${var.token} --accept-policies --insecure --sa-user ${var.service_account_user} --sa-password \"${var.service_account_password}\" --domain ${var.domain_name} --reg-code ${var.registration_code} ${var.ignore_disk_req ? "--ignore-disk-req" : ""} 2>&1 | tee output.txt",
+
+            "sudo docker service ls",
+        ]
+    }
+}
---
+++ modules/gcp/cac/vars.tf
@@ -0,0 +1,98 @@
+variable "prefix" {
+  description = "Prefix to add to name of new resources"
+  default = ""
+}
+variable "subnet" {
+  description = "Subnet to deploy the Cloud Access Connector"
+  type = "string"
+}
+
+variable "private_ip" {
+  description = "Static internal IP address for the Cloud Access Connector"
+  default = ""
+}
+
+variable "machine_type" {
+  description = "Machine type for Cloud Access Controller"
+  default = "n1-standard-2"
+}
+
+variable "disk_image_project" {
+  description = "Disk image project for Cloud Access Controller"
+  default = "ubuntu-os-cloud"
+}
+
+variable "disk_image_family" {
+  description = "Disk image family for Cloud Access Controller"
+  default = "ubuntu-1804-lts"
+}
+
+variable "disk_size_gb" {
+  description = "Disk size (GB) of Cloud Access Controller"
+  default = "50" 
+}
+
+variable "domain_name" {
+  description = "Name of the domain to join"
+  type = "string"
+}
+
+variable "domain_controller_ip" {
+  description = "Internal IP of the Domain Controller"
+  type = "string"
+}
+
+variable "cac_admin_user" {
+  description = "Username of Cloud Access Connector Administrator"
+  type = "string"
+}
+
+variable "cac_admin_password" {
+  description = "Password for Cloud Access Connector Administrator"
+  type = "string"
+}
+
+variable "cac_admin_ssh_pub_key_file" {
+  description = "SSH public key for Cloud Access Connector Administrator"
+  type = "string"
+}
+
+variable "cac_admin_ssh_priv_key_file" {
+  description = "SSH private key for Cloud Access Connector Administrator"
+  type = "string"
+}
+
+variable "cac_installer_url" {
+  description = "Location of Cloud Access Connector installer"
+  default = "https://teradici.bintray.com/cloud-access-connector/cloud-access-connector-0.1.1.tar.gz"
+}
+
+variable "cam_url" {
+  description = "cam server url."
+  default = "https://cam.teradici.com"
+}
+
+variable "token" {
+  description = "AUTH Token from CAM Service"
+  type = "string"
+}
+
+variable "service_account_user" {
+  description = "Active Directory Service Account username"
+  type = "string"
+}
+
+variable "service_account_password" {
+  description = "Active Directory Service Account password"
+  type = "string"
+}
+
+variable "registration_code" {
+  description = "PCoIP Registration code"
+  type = "string"
+}
+
+variable "ignore_disk_req" {
+  description = "Ignore the check for the minimum disk space requirement"
+  default = true
+}
