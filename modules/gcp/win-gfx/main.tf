169079e362952d1525023b1e631724378002e816
Sherman Yin <syin@teradici.com>
2019-07-09 14:19:17 -0700

    [5/12] Upgrade to Terraform 0.12
    Updates win-gfx module, also update disk image to
    windows-server-2016-dc-v20190620
    
    Signed-off-by: Sherman Yin <syin@teradici.com>

--- modules/gcp/win-gfx/main.tf
+++ modules/gcp/win-gfx/main.tf
@@ -5,130 +5,126 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-terraform {
-  required_version = "< 0.12"
-}
-
 locals {
-    prefix = "${var.prefix != "" ? "${var.prefix}-" : ""}"
-    # Windows computer names must be <= 15 characters, minus 3 chars for "-xy"
-    # where xy is number of instances (0-99)
-    # TODO: remove the min() function when Terraform 0.12 is available
-    host_name = "${substr("${local.prefix}${var.name}", 0, min(12, length(local.prefix)+length(var.name)))}"
-    setup_file = "C:/Temp/setup.ps1"
+  prefix = var.prefix != "" ? "${var.prefix}-" : ""
+
+  # Windows computer names must be <= 15 characters, minus 4 chars for "-xyz"
+  # where xyz is number of instances (0-999)
+  host_name = substr("${local.prefix}${var.name}", 0, 11)
+  setup_file = "C:/Temp/setup.ps1"
 }
 
 data "template_file" "sysprep-script" {
-    template = "${file("${path.module}/sysprep.ps1.tpl")}"
+  template = file("${path.module}/sysprep.ps1.tpl")
 
-    vars {
-        admin_password = "${var.admin_password}"
-    }
+  vars = {
+    admin_password = var.admin_password
+  }
 }
 
 data "template_file" "setup-script" {
-    template = "${file("${path.module}/setup.ps1.tpl")}"
+  template = file("${path.module}/setup.ps1.tpl")
 
-    vars {
-        nvidia_driver_location = "${var.nvidia_driver_location}"
-        nvidia_driver_filename = "${var.nvidia_driver_filename}"
-        pcoip_agent_location = "${var.pcoip_agent_location}"
-        pcoip_agent_filename = "${var.pcoip_agent_filename}"
-        pcoip_registration_code = "${var.pcoip_registration_code}"
-
-        gcp_project_id = "${var.gcp_project_id}"
-        domain_name = "${var.domain_name}"
-        domain_controller_ip = "${var.domain_controller_ip}"
-        service_account_username = "${var.service_account_username}"
-        service_account_password = "${var.service_account_password}"
-    }
+  vars = {
+    nvidia_driver_location   = var.nvidia_driver_location
+    nvidia_driver_filename   = var.nvidia_driver_filename
+    pcoip_agent_location     = var.pcoip_agent_location
+    pcoip_agent_filename     = var.pcoip_agent_filename
+    pcoip_registration_code  = var.pcoip_registration_code
+    gcp_project_id           = var.gcp_project_id
+    domain_name              = var.domain_name
+    domain_controller_ip     = var.domain_controller_ip
+    service_account_username = var.service_account_username
+    service_account_password = var.service_account_password
+  }
 }
 
 resource "google_compute_instance" "win-gfx" {
-    count = "${var.instance_count}"
+  count = var.instance_count
 
-    provider = "google"
-    name = "${local.host_name}-${count.index}"
-    machine_type = "${var.machine_type}"
+  provider     = google
+  name         = "${local.host_name}-${count.index}"
+  machine_type = var.machine_type
 
-    guest_accelerator {
-        type = "${var.accelerator_type}"
-        count = "${var.accelerator_count}"
+  guest_accelerator {
+    type  = var.accelerator_type
+    count = var.accelerator_count
+  }
+
+  # This is needed to prevent "Instances with guest accelerators do not support live migration" error
+  scheduling {
+    on_host_maintenance = "TERMINATE"
+  }
+
+  boot_disk {
+    initialize_params {
+      #image = "projects/${var.disk_image_project}/global/images/family/${var.disk_image_family}"
+      image = "projects/${var.disk_image_project}/global/images/${var.disk_image}"
+      type  = "pd-ssd"
+      size  = var.disk_size_gb
     }
+  }
 
-    # This is needed to prevent "Instances with guest accelerators do not support live migration" error
-    scheduling {
-        on_host_maintenance = "TERMINATE"
+  network_interface {
+    subnetwork = var.subnet
+    access_config {
     }
+  }
 
-    boot_disk {
-        initialize_params {
-            #image = "projects/${var.disk_image_project}/global/images/family/${var.disk_image_family}"
-            image = "projects/${var.disk_image_project}/global/images/${var.disk_image}"
-            type = "pd-ssd"
-            size = "${var.disk_size_gb}"
-        }
-    }
+  tags = [
+    "${local.prefix}tag-rdp",
+    "${local.prefix}tag-winrm",
+    "${local.prefix}tag-icmp",
+  ]
 
-    network_interface {
-        subnetwork = "${var.subnet}"
-        access_config = {}
-    }
-
-    tags = [
-        "${local.prefix}tag-rdp",
-        "${local.prefix}tag-winrm",
-        "${local.prefix}tag-icmp",
-    ]
-
-    metadata {
-        sysprep-specialize-script-ps1 = "${data.template_file.sysprep-script.rendered}"
-    }
+  metadata = {
+    sysprep-specialize-script-ps1 = data.template_file.sysprep-script.rendered
+  }
 }
 
 resource "null_resource" "upload-scripts" {
-    count = "${var.instance_count}"
+  count = var.instance_count
 
-    depends_on = ["google_compute_instance.win-gfx"]
-    triggers {
-        instance_id = "${google_compute_instance.win-gfx.*.instance_id[count.index]}"
-    }
+  depends_on = [google_compute_instance.win-gfx]
+  triggers = {
+    instance_id = google_compute_instance.win-gfx[count.index].instance_id
+  }
 
-    connection {
-        type = "winrm"
-        user = "Administrator"
-        password = "${var.admin_password}"
-        host = "${google_compute_instance.win-gfx.*.network_interface.0.access_config.0.nat_ip[count.index]}"
-        port = "5986"
-        https = true
-        insecure = true
-    }
+  connection {
+    type     = "winrm"
+    user     = "Administrator"
+    password = var.admin_password
+    host     = google_compute_instance.win-gfx[count.index].network_interface[0].access_config[0].nat_ip
+    port     = "5986"
+    https    = true
+    insecure = true
+  }
 
-    provisioner "file" {
-        content = "${data.template_file.setup-script.rendered}"
-        destination = "${local.setup_file}"
-    }
+  provisioner "file" {
+    content     = data.template_file.setup-script.rendered
+    destination = local.setup_file
+  }
 }
 
 resource "null_resource" "run-setup-script" {
-    count = "${var.instance_count}"
+  count = var.instance_count
 
-    depends_on = ["null_resource.upload-scripts"]
-    triggers {
-        instance_id = "${google_compute_instance.win-gfx.*.instance_id[count.index]}"
-    }
+  depends_on = [null_resource.upload-scripts]
+  triggers = {
+    instance_id = google_compute_instance.win-gfx[count.index].instance_id
+  }
 
-    connection {
-        type = "winrm"
-        user = "Administrator"
-        password = "${var.admin_password}"
-        host = "${google_compute_instance.win-gfx.*.network_interface.0.access_config.0.nat_ip[count.index]}"
-        port = "5986"
-        https = true
-        insecure = true
-    }
+  connection {
+    type     = "winrm"
+    user     = "Administrator"
+    password = var.admin_password
+    host     = google_compute_instance.win-gfx[count.index].network_interface[0].access_config[0].nat_ip
+    port     = "5986"
+    https    = true
+    insecure = true
+  }
 
-    provisioner "remote-exec" {
-        inline = ["powershell -file ${local.setup_file}"]
-    }
+  provisioner "remote-exec" {
+    inline = ["powershell -file ${local.setup_file}"]
+  }
 }
--- modules/gcp/win-gfx/vars.tf
+++ modules/gcp/win-gfx/vars.tf
@@ -7,72 +7,72 @@
 
 variable "prefix" {
   description = "Prefix to add to name of new resources. Must be <= 9 characters."
-  default = ""
+  default     = ""
 }
 
 variable "name" {
   description = "Basename of hostname of the workstation. Hostname will be <prefix>-<name>-<number>. Lower case only."
-  default = "gwin"
+  default     = "gwin"
 }
 
 variable "pcoip_registration_code" {
   description = "PCoIP Registration code from Teradici"
-  type = "string"
+  type        = string
 }
 
 variable "domain_name" {
   description = "Name of the domain to join"
-  type = "string"
+  type        = string
 }
 
 variable "domain_controller_ip" {
   description = "Internal IP Address of the Domain Controller"
-  type = "string"
+  type        = string
 }
 
 variable "service_account_username" {
   description = "Active Directory Service Account username"
-  type = "string"
+  type        = string
 }
 
 variable "service_account_password" {
   description = "Active Directory Service Account password"
-  type = "string"
+  type        = string
 }
 
 variable "gcp_project_id" {
   description = "GCP Project ID"
-  type = "string"
+  type        = string
 }
 
 variable "subnet" {
   description = "Subnet to deploy the Workstation"
-  type = "string"
+  type        = string
 }
 
 variable "instance_count" {
   description = "Number of Windows Graphics Workstations to deploy"
-  default = 1
+  default     = 1
 }
 
 variable "machine_type" {
   description = "Machine type for Workstation"
-  default = "n1-standard-2"
+  default     = "n1-standard-2"
 }
 
 variable "accelerator_type" {
   description = "Accelerator type for the Workstation"
-  default = "nvidia-tesla-p4-vws"
+  default     = "nvidia-tesla-p4-vws"
 }
 
 variable "accelerator_count" {
   description = "Number of GPUs for the Workstation"
-  default = "1"
+  default     = "1"
 }
 
 variable "disk_image_project" {
   description = "Disk image project for the Workstation"
-  default = "windows-cloud"
+  default     = "windows-cloud"
 }
 
 #variable "disk_image_family" {
@@ -81,35 +81,35 @@ variable "disk_image_project" {
 #}
 variable "disk_image" {
   description = "Disk image to use for the Workstation"
-  default = "windows-server-2016-dc-v20190312"
+  default     = "windows-server-2016-dc-v20190620"
 }
 
 variable "disk_size_gb" {
   description = "Disk size (GB) of the Workstation"
-  default = "100"
+  default     = "100"
 }
 
 variable "admin_password" {
   description = "Password for the Administrator of the Workstation"
-  type = "string"
+  type        = string
 }
 
 variable "nvidia_driver_location" {
   description = "URL of NVIDIA GRID driver location"
-  default = "https://storage.googleapis.com/nvidia-drivers-us-public/GRID/GRID7.1/"
+  default     = "https://storage.googleapis.com/nvidia-drivers-us-public/GRID/GRID7.1/"
 }
 
 variable "nvidia_driver_filename" {
   description = "Filename of NVIDIA GRID driver"
-  default = "412.16_grid_win10_server2016_64bit_international.exe"
+  default     = "412.16_grid_win10_server2016_64bit_international.exe"
 }
 
 variable "pcoip_agent_location" {
   description = "URL of Teradici PCoIP Graphics Agent"
-  default = "https://downloads.teradici.com/win/stable/"
+  default     = "https://downloads.teradici.com/win/stable/"
 }
 
 variable "pcoip_agent_filename" {
   description = "Filename of Teradici PCoIP Graphics Agent. Leave blank to download the latest."
-  default = ""
+  default     = ""
 }
