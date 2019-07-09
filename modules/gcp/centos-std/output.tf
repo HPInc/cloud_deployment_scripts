928ead6645b48e5a02d788f4118414cf5255b1d2
Sherman Yin <syin@teradici.com>
2019-07-09 14:22:57 -0700

    [7/12] Upgrade to Terraform 0.12
    Updates centos-std module, also updates disk image to centos-7-v20190619
    
    Signed-off-by: Sherman Yin <syin@teradici.com>

--- modules/gcp/centos-std/output.tf
+++ modules/gcp/centos-std/output.tf
@@ -6,9 +6,9 @@
  */
 
 output "internal-ip" {
-    value = ["${google_compute_instance.centos-std.*.network_interface.0.network_ip}"]
+  value = [google_compute_instance.centos-std[*].network_interface[0].network_ip]
 }
 
 output "public-ip" {
-    value = ["${google_compute_instance.centos-std.*.network_interface.0.access_config.0.nat_ip}"]
+  value = [google_compute_instance.centos-std[*].network_interface[0].access_config[0].nat_ip]
 }
