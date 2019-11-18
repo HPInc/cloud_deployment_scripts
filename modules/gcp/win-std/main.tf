1c75fd9bade7baee4ddebfdc492ba19b1c79fdaf
Sherman Yin <syin@teradici.com>
2019-11-18 14:10:44 -0800

    [1/3] Add support for GCP Windows Standard RW
    Copy win-gfx to win-std to start
    
    Signed-off-by: Sherman Yin <syin@teradici.com>

---
+++ modules/gcp/win-std/main.tf
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2019 Teradici Corporation
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+locals {
+  prefix = var.prefix != "" ? "${var.prefix}-" : ""
+
+  # Windows computer names must be <= 15 characters, minus 4 chars for "-xyz"
+  # where xyz is number of instances (0-999)
+  host_name = substr("${local.prefix}${var.name}", 0, 11)
+
+  enable_public_ip = var.enable_public_ip ? [true] : []
+  startup_script = "win-gfx-startup.ps1"
+}
+
+resource "google_storage_bucket_object" "win-gfx-startup-script" {
+  count = tonumber(var.instance_count) == 0 ? 0 : 1
+
+  name    = local.startup_script
+  bucket  = var.bucket_name
+  content = templatefile(
+    "${path.module}/${local.startup_script}.tmpl",
+    {
+      kms_cryptokey_id         = var.kms_cryptokey_id,
+      pcoip_agent_location     = var.pcoip_agent_location,
+      pcoip_agent_filename     = var.pcoip_agent_filename,
+      pcoip_registration_code  = var.pcoip_registration_code,
+
+      nvidia_driver_location   = var.nvidia_driver_location,
+      nvidia_driver_filename   = var.nvidia_driver_filename,
+
+      domain_name              = var.domain_name,
+      admin_password           = var.admin_password,
+      service_account_username = var.service_account_username,
+      service_account_password = var.service_account_password,
+    }
+  )
+}
+
+resource "google_compute_instance" "win-gfx" {
+  count = var.instance_count
+
+  provider     = google
+  name         = "${local.host_name}-${count.index}"
+  zone         = var.gcp_zone
+  machine_type = var.machine_type
+
+  guest_accelerator {
+    type  = var.accelerator_type
+    count = var.accelerator_count
+  }
+
+  # This is needed to prevent "Instances with guest accelerators do not support live migration" error
+  scheduling {
+    on_host_maintenance = "TERMINATE"
+  }
+
+  boot_disk {
+    initialize_params {
+      image = var.disk_image
+      type  = "pd-ssd"
+      size  = var.disk_size_gb
+    }
+  }
+
+  network_interface {
+    subnetwork = var.subnet
+
+    dynamic access_config {
+      for_each = local.enable_public_ip
+      content {}
+    }
+  }
+
+  tags = var.network_tags
+
+  metadata = {
+    windows-startup-script-url = "gs://${var.bucket_name}/${google_storage_bucket_object.win-gfx-startup-script[0].output_name}"
+  }
+
+  service_account {
+    email = var.gcp_service_account == "" ? null : var.gcp_service_account
+    scopes = ["cloud-platform"]
+  }
+}
---
+++ modules/gcp/win-std/vars.tf
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2019 Teradici Corporation
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+variable "gcp_service_account" {
+  description = "Service Account in the GCP Project"
+  type        = string
+}
+
+variable "prefix" {
+  description = "Prefix to add to name of new resources. Must be <= 9 characters."
+  default     = ""
+}
+
+variable "name" {
+  description = "Basename of hostname of the workstation. Hostname will be <prefix>-<name>-<number>. Lower case only."
+  default     = "gwin"
+}
+
+variable "pcoip_registration_code" {
+  description = "PCoIP Registration code from Teradici"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "Name of the domain to join"
+  type        = string
+}
+
+variable "service_account_username" {
+  description = "Active Directory Service Account username"
+  type        = string
+}
+
+variable "service_account_password" {
+  description = "Active Directory Service Account password"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of bucket to retrieve startup script."
+  type        = string
+}
+
+variable "gcp_zone" {
+  description = "Zone to deploy the Cloud Access Connector"
+  default     = "us-west2-b"
+}
+
+variable "subnet" {
+  description = "Subnet to deploy the Workstation"
+  type        = string
+}
+
+variable "enable_public_ip" {
+  description = "Assign a public IP to the Workstation"
+  default     = false
+}
+
+variable "network_tags" {
+  description = "Tags to be applied to the Workstation"
+  type        = list(string)
+}
+
+variable "instance_count" {
+  description = "Number of Windows Graphics Workstations to deploy"
+  default     = 1
+}
+
+variable "machine_type" {
+  description = "Machine type for Workstation"
+  default     = "n1-standard-4"
+}
+
+variable "accelerator_type" {
+  description = "Accelerator type for the Workstation"
+  default     = "nvidia-tesla-p4-vws"
+}
+
+variable "accelerator_count" {
+  description = "Number of GPUs for the Workstation"
+  default     = "1"
+}
+
+variable "disk_size_gb" {
+  description = "Disk size (GB) of the Workstation"
+  default     = "50"
+}
+
+variable "disk_image" {
+  description = "Disk image for the Workstation"
+  default     = "projects/windows-cloud/global/images/family/windows-2016"
+}
+
+variable "admin_password" {
+  description = "Password for the Administrator of the Workstation"
+  type        = string
+}
+
+variable "nvidia_driver_location" {
+  description = "URL of NVIDIA GRID driver location"
+  default     = "https://storage.googleapis.com/nvidia-drivers-us-public/GRID/GRID7.1/"
+}
+
+variable "nvidia_driver_filename" {
+  description = "Filename of NVIDIA GRID driver"
+  default     = "412.16_grid_win10_server2016_64bit_international.exe"
+}
+
+variable "pcoip_agent_location" {
+  description = "URL of Teradici PCoIP Graphics Agent"
+  default     = "https://downloads.teradici.com/win/stable/"
+}
+
+variable "pcoip_agent_filename" {
+  description = "Filename of Teradici PCoIP Graphics Agent. Leave blank to download the latest."
+  default     = ""
+}
+
+variable "depends_on_hack" {
+  description = "Workaround for Terraform Modules not supporting depends_on"
+  default     = []
+}
+
+variable "kms_cryptokey_id" {
+  description = "Resource ID of the KMS cryptographic key used to decrypt secrets, in the form of 'projects/<project-id>/locations/<location>/keyRings/<keyring-name>/cryptoKeys/<key-name>'"
+  default     = ""
+}
