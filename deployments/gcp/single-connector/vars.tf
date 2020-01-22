093fd611f1c574a62ee2500f3f910eac00c3159d
bsalla <bsalla@primesoft.net>
2020-01-22 15:00:20 +0530

    Updated windows server OS in all GCP vars.tf files

--- deployments/gcp/single-connector/vars.tf
+++ deployments/gcp/single-connector/vars.tf
@@ -69,7 +69,7 @@ variable "dc_disk_size_gb" {
 
 variable "dc_disk_image" {
   description = "Disk image for the Domain Controller"
-  default     = "projects/windows-cloud/global/images/windows-server-2016-dc-v20191210"
+  default     = "projects/windows-cloud/global/images/windows-server-2016-dc-v20200114"
 }
 
 variable "dc_admin_password" {
@@ -201,7 +201,7 @@ variable "win_gfx_disk_size_gb" {
 
 variable "win_gfx_disk_image" {
   description = "Disk image for the Windows Graphics Workstation"
-  default     = "projects/windows-cloud/global/images/windows-server-2016-dc-v20191210"
+  default     = "projects/windows-cloud/global/images/windows-server-2016-dc-v20200114"
 }
 
 variable "win_std_instance_count" {
@@ -221,7 +221,7 @@ variable "win_std_disk_size_gb" {
 
 variable "win_std_disk_image" {
   description = "Disk image for the Windows Standard Workstation"
-  default     = "projects/windows-cloud/global/images/windows-server-2016-dc-v20191210"
+  default     = "projects/windows-cloud/global/images/windows-server-2016-dc-v20200114"
 }
 
 variable "centos_gfx_instance_count" {
