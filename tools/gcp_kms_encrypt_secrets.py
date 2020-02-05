6767540d8e1729e197fe29eeebe74650158a9b32
Edwin Pau <epau@teradici.com>
2020-02-04 16:20:28 -0800

    Automate the creation of tfvars with encrypted secrets
    The python script that encrypts the secrets using the user provided
    GCP service account will now create a a new terraform.tfvars file
    with the encrypted secrets rendered into the appropriate fields.
    The script overwrites the existing tfvars file with plaintext secrets,
    and the result is a terraform.tfvars file that is ready to be used for
    deployments (both single and multi-region are supported).
    
    Signed-off-by: Edwin Pau <epau@teradici.com>

--- tools/gcp_kms_encrypt_secrets.py
+++ tools/gcp_kms_encrypt_secrets.py
@@ -5,36 +5,53 @@
 
 #!/usr/local/bin/python3
 
-from google.cloud import kms_v1
-from google.cloud.kms_v1 import enums
-import os
 import base64
 import json
-import ntpath
+from google.cloud.kms_v1 import enums
+from google.cloud        import kms_v1
+from google.oauth2       import service_account
+import os
 
-# Enter the path to the GCP credentials for the service account
-GCP_CREDENTIALS_FILE = "/path/to/gcp_cred.json"
+# Global variables
+GCP_CREDENTIALS_FILE = None
+CAM_CREDENTIALS_FILE = None
 
-# Enter the GCP project ID associated with the provided GCP service account
-PROJECT_ID = "your-project-1234"
+# Prompt user for deployment type (multi-region or single) and returns the path to the tfvars
+def get_tfvars_path():
+    validInput = False
+    deployment = None
 
-# Choose and enter new names for the key ring and crypto key.
-# Note: For security, keyring names cannot be changed or deleted once created.
-KEY_RING_ID = "create-a-key-ring-name"
-CRYPTO_KEY  = "create-a-crypto-key-name"
+    while not validInput:
+        deployment = input("Enter 0 or 1 for the deployment type:\n[0] for single-connector\n[1] for multi-region\n")
+        
+        if (deployment == "0") or (deployment == "1"):
+            validInput = True
+            print("")
 
-# Secrets to be encrypted using newly created crypto key
-DC_ADMIN_PASSWORD           = "SecuRe_pwd1"
-SAFE_MODE_ADMIN_PASSWORD    = "SecuRe_pwd2"
-AD_SERVICE_ACCOUNT_PASSWORD = "SecuRe_pwd3"
-PCOIP_REGISTRATION_CODE     = "ABCDEFGHIJKL@0123-4567-89AB-CDEF"
-CAM_CREDENTIALS_FILE        = "/path/to/cam_cred.json"
+    # Read the corresponding .tfvars file based on user selection
+    switcher = {
+        "0": "../deployments/gcp/single-connector/terraform.tfvars",
+        "1": "../deployments/gcp/multi-region/terraform.tfvars"
+    }
 
-# Set the location to use "global"
-LOCATION = "global"
+    return switcher.get(deployment)
 
-# Set environment variables to be used by KMS client
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GCP_CREDENTIALS_FILE
+
+# Read terraform.tfvars for user provided configurations
+def read_terraform_tfvars(tfvars_file):
+    # Declare an empty list
+    tf_data = {}
+
+    with open(tfvars_file, 'r') as f:
+        for line in f:
+            if line[0] in ('#', '\n'):
+                continue
+            
+            key, value = map(str.strip, line.split('='))
+            tf_data[key] = value
+
+    return tf_data
+
 
 # List KMS key rings
 def list_key_rings(client, project_id, location):
@@ -124,10 +141,6 @@ def encrypt_cam_credentials(client, project_id, location, key_ring_id, crypto_ke
     # CAM credentials file path to be encrypted
     cam_credentials_file = CAM_CREDENTIALS_FILE
 
-    # Directory and file name of CAM JSON credentials
-    cam_credentials_dirname  = ntpath.dirname(cam_credentials_file)
-    cam_credentials_basename = ntpath.basename(cam_credentials_file)
-
     # Encrypt CAM JSON credentials
     try:
         with open(cam_credentials_file) as cam_credentials:
@@ -136,7 +149,7 @@ def encrypt_cam_credentials(client, project_id, location, key_ring_id, crypto_ke
         cam_credentials_string = json.dumps(cam_credentials)
         cam_credentials_encrypted = encrypt_plaintext(client, project_id, location, key_ring_id, crypto_key_id,
                                     cam_credentials_string)
-        print("Encrypted CAM credentials:")
+        print("Encrypting CAM credentials...")
         print("{}\n".format(cam_credentials_encrypted))
 
         with open("{}.encrypted".format(cam_credentials_file), "w") as cam_credentials_encrypted_file:
@@ -148,24 +161,63 @@ def encrypt_cam_credentials(client, project_id, location, key_ring_id, crypto_ke
         print("")
 
 
-# Driver of this script
+# Write the new tfvars file using encrypted secrets
+def write_new_tfvars(tfvars_file, secrets, kms_cryptokey_id):
+    lines = []
+    
+    with open(tfvars_file, 'r') as f:
+        for line in f:
+            if "kms_keyring_name" in line:
+                continue
+            if "kms_cryptokey_name" in line:
+                line = "kms_cryptokey_id = \"{}\"".format(kms_cryptokey_id)
+            if "dc_admin_password" in line:
+                line = "dc_admin_password           = \"{}\"".format(secrets.get("dc_admin_password"))
+            if "safe_mode_admin_password" in line:
+                line = "safe_mode_admin_password    = \"{}\"".format(secrets.get("safe_mode_admin_password"))
+            if "ad_service_account_password" in line:
+                line = "ad_service_account_password = \"{}\"".format(secrets.get("ad_service_account_password"))
+            if "pcoip_registration_code" in line:
+                line = "pcoip_registration_code     = \"{}\"".format(secrets.get("pcoip_registration_code"))    
+            if "cam_credentials_file" in line:
+                line = "cam_credentials_file        = \"{}.encrypted\"".format(CAM_CREDENTIALS_FILE)    
+            lines.append(line.rstrip())
+    
+    # Rewrite the existing terraform.tfvars
+    with open("terraform.tfvars", 'w') as f:
+        f.writelines("%s\n" %line for line in lines)
+
+
 def main():
+    # Path to tfvars depends on user input (multi or single)
+    tfvars_file = get_tfvars_path()
+    
+    # Read tfvars file into a dictionary
+    tfvars = read_terraform_tfvars(tfvars_file)
+
+    # Set global variables using tfvars file
+    global GCP_CREDENTIALS_FILE
+    GCP_CREDENTIALS_FILE = tfvars.get("gcp_credentials_file").replace('\"', '')
+    global CAM_CREDENTIALS_FILE
+    CAM_CREDENTIALS_FILE = tfvars.get("cam_credentials_file").replace('\"', '')
+
     # Create an API client for the KMS API using the provided GCP service account
-    client = kms_v1.KeyManagementServiceClient()
+    credentials     = service_account.Credentials.from_service_account_file(GCP_CREDENTIALS_FILE)
+    client          = kms_v1.KeyManagementServiceClient( credentials = credentials )
 
-    # Resource names of the location associated with the key rings
-    project_id    = PROJECT_ID
-    key_ring_id   = KEY_RING_ID
-    crypto_key_id = CRYPTO_KEY
-    location      = LOCATION
-
-    # Secrets in plaintext
+    # Secrets in plaintext to be encrypted
     secrets = {
-        "dc_admin_password"           : DC_ADMIN_PASSWORD,
-        "safe_mode_admin_password"    : SAFE_MODE_ADMIN_PASSWORD,
-        "ad_service_account_password" : AD_SERVICE_ACCOUNT_PASSWORD,
-        "pcoip_registration_code"     : PCOIP_REGISTRATION_CODE
+        "dc_admin_password"           : tfvars.get("dc_admin_password"),
+        "safe_mode_admin_password"    : tfvars.get("safe_mode_admin_password"),
+        "ad_service_account_password" : tfvars.get("ad_service_account_password"),
+        "pcoip_registration_code"     : tfvars.get("pcoip_registration_code")
     }
+    
+    # GCP KMS resource variables
+    project_id       = tfvars.get("gcp_project_id").replace('\"', '')
+    key_ring_id      = tfvars.get("kms_keyring_name").replace('\"', '')
+    crypto_key_id    = tfvars.get("kms_cryptokey_name").replace('\"', '')
+    location         = "global"
 
     # Create a key ring
     try:
@@ -189,16 +241,15 @@ def main():
         print("An exception occurred creating new crypto key:")
         print(err)
         print("")
-    
+
     # Encrypt secrets including CAM credentials
     try:
         for secret in secrets:
             print("Encrypting {}...".format(secret))
-
             print("{0}: {1}".format(secret, secrets.get(secret)))
-
             ciphertext = encrypt_plaintext(client, project_id, location, key_ring_id, crypto_key_id, secrets.get(secret))
             print("{0}: {1}\n".format(secret, ciphertext))
+            secrets[secret] = ciphertext
 
         encrypt_cam_credentials(client, project_id, location, key_ring_id, crypto_key_id)
             
@@ -206,7 +257,10 @@ def main():
         print("An exception occurred encrypting secrets:")
         print(err)
         print("")
-      
+
+    # Write secrets into new terraform.tfvars file
+    kms_cryptokey_id = client.crypto_key_path_path(project_id, location, key_ring_id, crypto_key_id)
+    write_new_tfvars(tfvars_file, secrets, kms_cryptokey_id)
 
 # Entry point to this script
 if __name__ == '__main__':
