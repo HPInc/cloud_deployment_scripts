10a11fea6620689fd36b936c9b33e135d8dcc6bd
bsalla <bsalla@primesoft.net>
2020-02-17 15:46:13 +0530

    Updated windows server OS in all GCP vars.tf files

--- deployments/gcp/dc-only/vars.tf
+++ deployments/gcp/dc-only/vars.tf
@@ -69,7 +69,7 @@ variable "dc_disk_size_gb" {
 
 variable "dc_disk_image" {
   description = "Disk image for the Domain Controller"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20200114"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20200211"
 }
 
 variable "dc_admin_password" {
