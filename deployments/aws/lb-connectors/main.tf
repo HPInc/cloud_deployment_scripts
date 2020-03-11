6479e55cfd0af8cda6cd446ced3416a26f3ee3a1
Sherman Yin <syin@teradici.com>
2020-03-11 13:33:16 -0700

    AWS: adds lb-connector deployment
    Added lb-connector deployment where connectors from multiple
    availability zones running behind an AWS ALB.
    
    Signed-off-by: Sherman Yin <syin@teradici.com>

---
+++ deployments/aws/lb-connectors/main.tf
@@ -0,0 +1,320 @@
+/*
+ * Copyright (c) 2020 Teradici Corporation
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+locals {
+  prefix = var.prefix != "" ? "${var.prefix}-" : ""
+  bucket_name = "${local.prefix}pcoip-scripts-${random_id.bucket-name.hex}"
+}
+
+resource "random_id" "bucket-name" {
+  byte_length = 3
+}
+
+resource "aws_s3_bucket" "scripts" {
+  bucket        = local.bucket_name
+  region        = var.aws_region
+  acl           = "private"
+  force_destroy = true
+
+  tags = {
+    Name = local.bucket_name
+  }
+}
+
+module "dc" {
+  source = "../../../modules/aws/dc"
+
+  prefix = var.prefix
+
+  customer_master_key_id      = var.customer_master_key_id
+  domain_name                 = var.domain_name
+  admin_password              = var.dc_admin_password
+  safe_mode_admin_password    = var.safe_mode_admin_password
+  ad_service_account_username = var.ad_service_account_username
+  ad_service_account_password = var.ad_service_account_password
+  domain_users_list           = var.domain_users_list
+
+  bucket_name        = aws_s3_bucket.scripts.id
+  subnet             = aws_subnet.dc-subnet.id
+  private_ip         = var.dc_private_ip
+  security_group_ids = [
+    data.aws_security_group.default.id,
+    aws_security_group.allow-rdp.id,
+    aws_security_group.allow-winrm.id,
+    aws_security_group.allow-icmp.id,
+  ]
+
+  instance_type = var.dc_instance_type
+  disk_size_gb  = var.dc_disk_size_gb
+
+  ami_owner = var.dc_ami_owner
+  ami_name  = var.dc_ami_name
+}
+
+resource "aws_key_pair" "cam_admin" {
+  key_name   = var.admin_ssh_key_name
+  public_key = file(var.admin_ssh_pub_key_file)
+}
+
+resource "aws_lb" "cac-alb" {
+  name               = "${local.prefix}cac-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [
+    data.aws_security_group.default.id,
+    aws_security_group.allow-ssh.id,
+    aws_security_group.allow-icmp.id,
+    aws_security_group.allow-https.id,
+    aws_security_group.allow-pcoip.id,
+  ]
+  subnets            = aws_subnet.cac-subnets[*].id
+}
+
+resource "aws_lb_target_group" "cac-tg" {
+  name        = "${local.prefix}cac-tg"
+  port        = 443
+  protocol    = "HTTPS"
+  target_type = "instance"
+  vpc_id      = aws_vpc.vpc.id
+
+  stickiness {
+    type = "lb_cookie"
+  }
+
+  #TODO add health check
+}
+
+resource "tls_private_key" "priv-key" {
+  algorithm   = "RSA"
+}
+
+resource "tls_self_signed_cert" "ss-cert" {
+  key_algorithm   = "RSA"
+  private_key_pem = tls_private_key.priv-key.private_key_pem
+
+  subject {
+    common_name  = var.domain_name
+  }
+
+  # 90 days, like letsencrypt
+  validity_period_hours = 2160
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+}
+
+resource "aws_acm_certificate" "ssl-cert" {
+  private_key      = tls_private_key.priv-key.private_key_pem
+  certificate_body = tls_self_signed_cert.ss-cert.cert_pem
+
+  tags = {
+    Name = "${local.prefix}ssl-cert"
+  }
+}
+
+resource "aws_lb_listener" "alb-listener" {
+  load_balancer_arn = aws_lb.cac-alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.ssl-cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.cac-tg.arn
+  }
+}
+
+module "cac" {
+  source = "../../../modules/aws/cac"
+
+  prefix = var.prefix
+
+  aws_region              = var.aws_region
+  customer_master_key_id  = var.customer_master_key_id
+  cam_url                 = var.cam_url
+  pcoip_registration_code = var.pcoip_registration_code
+  cac_token               = var.cac_token
+
+  domain_name                 = var.domain_name
+  domain_controller_ip        = module.dc.internal-ip
+  ad_service_account_username = var.ad_service_account_username
+  ad_service_account_password = var.ad_service_account_password
+
+  zone_list           = aws_subnet.cac-subnets[*].availability_zone
+  subnet_list         = aws_subnet.cac-subnets[*].id
+  instance_count_list = var.cac_instance_count_list
+
+  security_group_ids = [
+    data.aws_security_group.default.id,
+    aws_security_group.allow-ssh.id,
+    aws_security_group.allow-icmp.id,
+    aws_security_group.allow-https.id,
+    aws_security_group.allow-pcoip.id,
+  ]
+
+  bucket_name    = aws_s3_bucket.scripts.id
+  instance_type  = var.cac_instance_type
+  disk_size_gb   = var.cac_disk_size_gb
+
+  ami_owner = var.cac_ami_owner
+  ami_name  = var.cac_ami_name
+
+  admin_ssh_key_name = var.admin_ssh_key_name
+}
+
+resource "aws_lb_target_group_attachment" "cac-tg-attachement" {
+  count            = length(module.cac.instance-id)
+
+  target_group_arn = aws_lb_target_group.cac-tg.arn
+  target_id        = module.cac.instance-id[count.index]
+  port             = 443
+}
+
+module "win-gfx" {
+  source = "../../../modules/aws/win-gfx"
+
+  instance_count = var.win_gfx_instance_count
+
+  prefix = var.prefix
+
+  customer_master_key_id = var.customer_master_key_id
+
+  pcoip_registration_code = var.pcoip_registration_code
+
+  domain_name                 = var.domain_name
+  admin_password              = var.dc_admin_password
+  ad_service_account_username = var.ad_service_account_username
+  ad_service_account_password = var.ad_service_account_password
+
+  bucket_name        = aws_s3_bucket.scripts.id
+  subnet             = aws_subnet.ws-subnet.id
+  enable_public_ip   = var.enable_workstation_public_ip
+  security_group_ids = [
+    data.aws_security_group.default.id,
+    aws_security_group.allow-icmp.id,
+    aws_security_group.allow-rdp.id,
+  ]
+
+  instance_type     = var.win_gfx_instance_type
+  disk_size_gb      = var.win_gfx_disk_size_gb
+
+  ami_owner = var.win_gfx_ami_owner
+  ami_name  = var.win_gfx_ami_name
+
+  depends_on_hack = [aws_nat_gateway.nat.id]
+}
+
+module "win-std" {
+  source = "../../../modules/aws/win-std"
+
+  instance_count = var.win_std_instance_count
+
+  prefix = var.prefix
+
+  customer_master_key_id = var.customer_master_key_id
+
+  pcoip_registration_code = var.pcoip_registration_code
+
+  domain_name                 = var.domain_name
+  admin_password              = var.dc_admin_password
+  ad_service_account_username = var.ad_service_account_username
+  ad_service_account_password = var.ad_service_account_password
+
+  bucket_name        = aws_s3_bucket.scripts.id
+  subnet             = aws_subnet.ws-subnet.id
+  enable_public_ip   = var.enable_workstation_public_ip
+  security_group_ids = [
+    data.aws_security_group.default.id,
+    aws_security_group.allow-icmp.id,
+    aws_security_group.allow-rdp.id,
+  ]
+
+  instance_type     = var.win_std_instance_type
+  disk_size_gb      = var.win_std_disk_size_gb
+
+  ami_owner = var.win_std_ami_owner
+  ami_name  = var.win_std_ami_name
+
+  depends_on_hack = [aws_nat_gateway.nat.id]
+}
+
+module "centos-gfx" {
+  source = "../../../modules/aws/centos-gfx"
+
+  instance_count = var.centos_gfx_instance_count
+
+  prefix = var.prefix
+
+  customer_master_key_id = var.customer_master_key_id
+
+  pcoip_registration_code = var.pcoip_registration_code
+
+  domain_name                 = var.domain_name
+  domain_controller_ip        = module.dc.internal-ip
+  ad_service_account_username = var.ad_service_account_username
+  ad_service_account_password = var.ad_service_account_password
+
+  bucket_name        = aws_s3_bucket.scripts.id
+  subnet             = aws_subnet.ws-subnet.id
+  enable_public_ip   = var.enable_workstation_public_ip
+  security_group_ids = [
+    data.aws_security_group.default.id,
+    aws_security_group.allow-icmp.id,
+    aws_security_group.allow-ssh.id,
+  ]
+
+  instance_type     = var.centos_gfx_instance_type
+  disk_size_gb      = var.centos_gfx_disk_size_gb
+
+  ami_owner        = var.centos_gfx_ami_owner
+  ami_product_code = var.centos_gfx_ami_product_code
+  ami_name         = var.centos_gfx_ami_name
+
+  admin_ssh_key_name = var.admin_ssh_key_name
+
+  depends_on_hack = [aws_nat_gateway.nat.id]
+}
+
+module "centos-std" {
+  source = "../../../modules/aws/centos-std"
+
+  instance_count = var.centos_std_instance_count
+
+  prefix = var.prefix
+
+  customer_master_key_id = var.customer_master_key_id
+
+  pcoip_registration_code = var.pcoip_registration_code
+
+  domain_name                 = var.domain_name
+  domain_controller_ip        = module.dc.internal-ip
+  ad_service_account_username = var.ad_service_account_username
+  ad_service_account_password = var.ad_service_account_password
+
+  bucket_name        = aws_s3_bucket.scripts.id
+  subnet             = aws_subnet.ws-subnet.id
+  enable_public_ip   = var.enable_workstation_public_ip
+  security_group_ids = [
+    data.aws_security_group.default.id,
+    aws_security_group.allow-icmp.id,
+    aws_security_group.allow-ssh.id,
+  ]
+
+  instance_type     = var.centos_std_instance_type
+  disk_size_gb      = var.centos_std_disk_size_gb
+
+  ami_owner        = var.centos_std_ami_owner
+  ami_product_code = var.centos_std_ami_product_code
+  ami_name         = var.centos_std_ami_name
+
+  admin_ssh_key_name = var.admin_ssh_key_name
+
+  depends_on_hack = [aws_nat_gateway.nat.id]
+}
---
+++ deployments/aws/lb-connectors/vars.tf
@@ -0,0 +1,286 @@
+/*
+ * Copyright (c) 2020 Teradici Corporation
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+variable "aws_credentials_file" {
+    description = "Location of AWS credentials file"
+    type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-west-1"
+}
+
+variable "prefix" {
+  description = "Prefix to add to name of new resources. Must be <= 9 characters."
+  default     = ""
+}
+
+variable "allowed_cidr_blks" {
+  description = "Open VPC firewall to allow ICMP, SSH, WinRM and RDP from these CIDR blocks. e.g. ['a.b.c.d/32', 'e.f.g.0/24']"
+  default     = []
+}
+
+variable "vpc_name" {
+  description = "Name of VPC to create"
+  default     = "vpc-cas"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for the VPC containing the CAS deployment"
+  default     = "10.0.0.0/16" 
+}
+
+variable "dc_subnet_cidr" {
+  description = "CIDR for subnet containing the Domain Controller"
+  default     = "10.0.0.0/24"
+}
+
+variable "dc_private_ip" {
+  description = "Static internal IP address for the Domain Controller"
+  default     = "10.0.0.100"
+}
+
+variable "dc_instance_type" {
+  description = "Instance type for the Domain Controller"
+  default     = "t2.xlarge"
+}
+
+variable "dc_disk_size_gb" {
+  description = "Disk size (GB) of the Domain Controller"
+  default     = "50"
+}
+
+variable "dc_ami_owner" {
+  description = "Owner of AMI for the Domain Controller"
+  default     = "amazon"
+}
+
+variable "dc_ami_name" {
+  description = "Name of the Windows AMI to create workstation from"
+  default     = "Windows_Server-2019-English-Full-Base-2020.03.11"
+}
+
+variable "domain_name" {
+  description = "Domain name for the new domain"
+  default     = "example.com"
+}
+
+variable "dc_admin_password" {
+  description = "Password for the Administrator of the Domain Controller"
+  type        = string
+}
+
+variable "safe_mode_admin_password" {
+  description = "Safe Mode Admin Password (Directory Service Restore Mode - DSRM)"
+  type        = string
+}
+
+variable "ad_service_account_username" {
+  description = "Active Directory Service account name to be created"
+  default     = "cam_admin"
+}
+
+variable "ad_service_account_password" {
+  description = "Active Directory Service account password"
+  type        = string
+}
+
+variable "domain_users_list" {
+  description = "Active Directory users to create, in CSV format"
+  type        = string
+  default     = ""
+}
+
+variable "cac_zone_list" {
+  description = "Zones in which to deploy Connectors"
+  default     = ["us-west-1a", "us-west-1c"]
+}
+
+variable "cac_subnet_cidr_list" {
+  description = "CIDRs for subnet containing the Cloud Access Connector"
+  default     = ["10.0.1.0/25", "10.0.1.128/25"]
+}
+
+variable "cac_instance_count_list" {
+  description = "Number of Cloud Access Connector instances to deploy in each region"
+  default     = [1, 1]
+}
+
+variable "cac_instance_type" {
+  description = "Instance type for the Cloud Access Connector"
+  default     = "t2.xlarge"
+}
+
+variable "cac_disk_size_gb" {
+  description = "Disk size (GB) of the Cloud Access Connector"
+  default     = "50"
+}
+
+variable "cac_ami_owner" {
+  description = "Owner of AMI for the Cloud Access Connector"
+  default     = "099720109477"
+}
+
+variable "cac_ami_name" {
+  description = "Name of the AMI to create Cloud Access Connector from"
+  default = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20200311"
+}
+
+variable "admin_ssh_key_name" {
+  description = "Name of Admin SSH Key"
+  default     = "cas_admin"
+}
+
+variable "admin_ssh_pub_key_file" {
+  description = "Admin SSH public key file"
+  type        = string
+}
+
+variable "cac_token" {
+  description = "Connector Token from CAM Service"
+  type        = string
+}
+
+variable "pcoip_registration_code" {
+  description = "PCoIP Registration code"
+  type        = string
+}
+
+variable "cam_url" {
+  description = "cam server url."
+  default     = "https://cam.teradici.com"
+}
+
+variable "ws_subnet_cidr" {
+  description = "CIDR for subnet containing Remote Workstations"
+  default     = "10.0.2.0/24"
+}
+
+variable "enable_workstation_public_ip" {
+  description = "Enable public IP for Workstations"
+  default     = false
+}
+
+variable "win_gfx_instance_count" {
+  description = "Number of Windows Graphics Workstations"
+  default     = 0
+}
+
+# G4s are Tesla T4s
+# G3s are M60
+variable "win_gfx_instance_type" {
+  description = "Instance type for the Windows Graphics Workstations"
+  default     = "g4dn.xlarge"
+}
+
+variable "win_gfx_disk_size_gb" {
+  description = "Disk size (GB) of the Windows Graphics Workstations"
+  default     = "50"
+}
+
+variable "win_gfx_ami_owner" {
+  description = "Owner of AMI for the Windows Graphics Workstations"
+  default     = "aws-marketplace"
+}
+
+variable "win_gfx_ami_name" {
+  description = "Name of the Windows AMI to create workstation from"
+  default     = "nvOffer-grid9.2-nv-windows-server-2019-QvWS-432.08-v201911180035*"
+}
+
+variable "win_std_instance_count" {
+  description = "Number of Windows Standard Workstations"
+  default     = 0
+}
+
+variable "win_std_instance_type" {
+  description = "Instance type for the Windows Standard Workstations"
+  default     = "t2.xlarge"
+}
+
+variable "win_std_disk_size_gb" {
+  description = "Disk size (GB) of the Windows Standard Workstations"
+  default     = "50"
+}
+
+variable "win_std_ami_owner" {
+  description = "Owner of AMI for the Windows Standard Workstations"
+  default     = "amazon"
+}
+
+variable "win_std_ami_name" {
+  description = "Name of the Windows AMI to create workstation from"
+  default     = "Windows_Server-2019-English-Full-Base-2020.03.11"
+}
+
+variable "centos_gfx_instance_count" {
+  description = "Number of CentOS Graphics Workstations"
+  default     = 0
+}
+
+# G4s are Tesla T4s
+# G3s are M60
+variable "centos_gfx_instance_type" {
+  description = "Instance type for the CentOS Graphics Workstations"
+  default     = "g4dn.xlarge"
+}
+
+variable "centos_gfx_disk_size_gb" {
+  description = "Disk size (GB) of the CentOS Graphics Workstations"
+  default     = "50"
+}
+
+variable "centos_gfx_ami_owner" {
+  description = "Owner of AMI for the CentOS Graphics Workstations"
+  default     = "aws-marketplace"
+}
+
+variable "centos_gfx_ami_product_code" {
+  description = "Product Code of the AMI for the CentOS Graphics Workstation"
+  default     = "aw0evgkw8e5c1q413zgy5pjce"
+}
+
+variable "centos_gfx_ami_name" {
+  description = "Name of the CentOS AMI to create workstation from"
+  default     = "CentOS Linux 7 x86_64 HVM EBS ENA 2002*"
+}
+
+variable "centos_std_instance_count" {
+  description = "Number of CentOS Standard Workstations"
+  default     = 0
+}
+
+variable "centos_std_instance_type" {
+  description = "Instance type for the CentOS Standard Workstations"
+  default     = "t2.xlarge"
+}
+
+variable "centos_std_disk_size_gb" {
+  description = "Disk size (GB) of the CentOS Standard Workstations"
+  default     = "50"
+}
+
+variable "centos_std_ami_owner" {
+  description = "Owner of AMI for the CentOS Standard Workstations"
+  default     = "aws-marketplace"
+}
+
+variable "centos_std_ami_product_code" {
+  description = "Product Code of the AMI for the CentOS Standard Workstation"
+  default     = "aw0evgkw8e5c1q413zgy5pjce"
+}
+
+variable "centos_std_ami_name" {
+  description = "Name of the CentOS AMI to create workstation from"
+  default     = "CentOS Linux 7 x86_64 HVM EBS ENA 2002*"
+}
+
+variable "customer_master_key_id" {
+  description = "The ID of the AWS KMS Customer Master Key used to decrypt secrets"
+  default     = ""
+}
