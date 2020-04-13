815b2ff41b450726d45248c1a91c60bc5887a2e0
Edwin-Pau <epau@teradici.com>
2020-04-13 13:08:47 -0600

    GCP: Add idle shutdown (multi-region)
    Added variables used for idle shutdown at the deployment level for all
    four workstations. The three variables implemented allow users to
    enable or disable idle shutdown for the workstations, set the idle
    timer before shutdown in minutes, and set the CPU polling interval in
    minutes.
    
    Signed-off-by: Edwin-Pau <epau@teradici.com>

--- deployments/gcp/multi-region/vars.tf
+++ deployments/gcp/multi-region/vars.tf
@@ -199,6 +199,21 @@ variable "enable_workstation_public_ip" {
   default     = false
 }
 
+variable "enable_workstation_idle_shutdown" {
+  description = "Enable Cloud Access Manager auto idle shutdown for Workstations"
+  default     = true
+}
+
+variable "minutes_idle_before_shutdown" {
+  description = "Minimum idle time for Workstations before auto idle shutdown, must be between 5 and 10000"
+  default     = 240
+}
+
+variable "minutes_cpu_polling_interval" {
+  description = "Polling interval for checking CPU utilization to determine if machine is idle, must be between 1 and 60"
+  default     = 15
+}
+
 variable "win_gfx_instance_count" {
   description = "Number of Windows Grpahics Workstations"
   default     = 0
