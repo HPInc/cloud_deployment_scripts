1c2324c9bcebffe2249dd69ce42fb13cbe3fec4c
Edwin-Pau <epau@teradici.com>
2020-05-06 20:01:56 -0600

    Update docstrings
    Updated and fixed all docstrings for the encryption script.
    
    Signed-off-by: Edwin-Pau <epau@teradici.com>

--- tools/kms_secrets_encryption.py
+++ tools/kms_secrets_encryption.py
@@ -32,21 +32,20 @@ class Tfvars_Parser:
         Dictionary containing key value pairs for all terraform.tfvars secrets.
     max_key_length : int 
         Longest string length of a tfvars_secrets key used to write secrets left-justified.
-    aws_credentials_file : str
-        Path to the AWS credentials file used for AWS KMS.
 
     Methods
     -------
+        __init__(tfvars_path)
         read_tfvars(tfvars_file)
     """
 
     def __init__(self, tfvars_path):
-        """Tfvars_Parser Class Constructor to initialize the object.
+        """Tfvars_Parser class constructor to initialize the object.
         
         Args
         ---- 
         tfvars_path : str
-            Path to the terraform.tfvars file
+            Path to the terraform.tfvars file being parsed.
         """
 
         # Read tfvars data and secrets into dictionaries
@@ -65,7 +64,7 @@ class Tfvars_Parser:
         Args
         ----
         tfvars_file : str
-            Path to a terraform.tfvars file
+            Path to the terraform.tfvars file being parsed.
 
         Returns
         -------
@@ -91,7 +90,7 @@ class Tfvars_Parser:
                 # "not line" must come first using short circuiting to avoid string index out of range error
                 if not line or line[0] in ("#"):
                     continue
-                
+
                 # Split the line into key value pairs using the first delimiter
                 key, value = map(str.strip, line.split('=', 1))
 
@@ -105,8 +104,8 @@ class Tfvars_Parser:
 
 class Tfvars_Encryptor(ABC):
     """This is an abstract super class that is inherited by 
-    Tfvars_Encryptor_AWS and Tfvars_Encryptor_GCP.
-    
+    AWS_Tfvars_Encryptor and GCP_Tfvars_Encryptor.
+
     It contains common attributes and methods that are used by the sub 
     encryptor classes to automate the encryption and decryption of 
     terraform.tfvars files.
@@ -120,16 +119,13 @@ class Tfvars_Encryptor(ABC):
         Instance of Key Management Service Client.
     credentials_file : str
         Path to the KMS client credentials file.
-    crypto_key_id : str
-        Defaulted to use "cas_key" as a crypto key ID.
 
     Methods
     -------
     Abstract methods:
-        __init__(self, tfvars_path)
+        __init__(tfvars_parser)
         create_crypto_key(crypto_key_id) 
         decrypt_ciphertext(ciphertext)
-        encrypt_file(file_path)
         encrypt_plaintext(plaintext)
         get_crypto_keys()
         initialize_cryptokey(crypto_key_id)
@@ -145,11 +141,11 @@ class Tfvars_Encryptor(ABC):
     @abstractmethod
     def __init__(self, tfvars_parser):
         """Tfvars_Encryptor class constructor to initialize the object.
-        
+
         Args
         ----
         tfvars_parser : object 
-            Instance of a Tfvars_Parser class.
+            Instance of Tfvars_Parser class.
         """
 
         self.tfvars_parser    = tfvars_parser
@@ -178,7 +174,7 @@ class Tfvars_Encryptor(ABC):
 
         Uses the KMS client to decrypt ciphertext back to plaintext using the 
         provided symmetric crypto key that belongs to this instance.
-        
+
         Args
         ----
         file_path : str
@@ -195,7 +191,7 @@ class Tfvars_Encryptor(ABC):
 
             with open(file_path) as f:
                 f_ciphertext = f.read()
-        
+
             f_plaintext = self.decrypt_ciphertext(f_ciphertext)
 
             # Removes the .encrypted appended using this encryptor
@@ -208,7 +204,7 @@ class Tfvars_Encryptor(ABC):
             print("An exception occurred decrypting file.")
             print("{}\n".format(err))
             raise SystemExit()
-        
+
         return file_path_decrypted
 
 
@@ -216,8 +212,8 @@ class Tfvars_Encryptor(ABC):
         """A method that decrypts the secrets contained in the terraform.tfvars file.
 
         This method contains the logic for handling the decryption of the secrets 
-        and any file paths associated with it using GCP KMS. Once decrypted, it calls 
-        write_new_tfvars() to write all secrets to a new terraform.tfvars file. 
+        and any file paths associated with it using the KMS client. Once decrypted, it 
+        calls write_new_tfvars() to write all secrets to a new terraform.tfvars file. 
         """
 
         # GCP uses kms_cryptokey_id while AWS uses customer_master_key_id
@@ -252,7 +248,7 @@ class Tfvars_Encryptor(ABC):
 
         Uses the KMS client to encrypt the plaintext in a file to ciphertext using 
         the provided symmetric crypto key that belongs to this instance.
-        
+
         Args
         ----
         file_path : str
@@ -280,7 +276,7 @@ class Tfvars_Encryptor(ABC):
             print("An exception occurred encrypting the file:")
             print("{}\n".format(err))
             raise SystemExit()
-        
+
         return file_path_encrypted
 
 
@@ -288,9 +284,9 @@ class Tfvars_Encryptor(ABC):
         """A method that encrypts secrets contained in the terraform.tfvars file.
 
         This method contains the logic for handling the encryption of the secrets 
-        and any file paths associated with it using GCP KMS. Once encrypted, it calls 
-        write_new_tfvars() to write all secrets to a new terraform.tfvars file. 
-        """    
+        and any file paths associated with it using the KMS client. Once encrypted, it 
+        calls write_new_tfvars() to write all secrets to a new terraform.tfvars file. 
+        """
 
         # Encrypt all secrets found in the tfvars_secrets dictionary
         try:
@@ -313,7 +309,7 @@ class Tfvars_Encryptor(ABC):
 
 
     def write_new_tfvars(self):
-        """A method that writes a new terraform.tfvars file
+        """A method that writes a new terraform.tfvars file.
 
         This method writes a new terraform.tfvars file that is ready to be used by
         Terraform after encrypting or decrypting. 
@@ -389,21 +385,30 @@ class GCP_Tfvars_Encryptor(Tfvars_Encryptor):
         Defaulted to use "global" as the location.
     key_ring_id : str
         Defaulted to use "cas_keyring" as a key ring ID.
+    crypto_key_id : str
+        Defaulted to use "cas_key" as the crypto key ID.
     crypto_key_path : str
         Full GCP resource path to the crypto key being used to encrypt and decrypt.
 
     Methods
     -------
-        __init__(self, tfvars_parser)
+        __init__(tfvars_parser)
+        create_crypto_key(crypto_key_id)
+        decrypt_ciphertext(ciphertext)
+        encrypt_plaintext(plaintext)
+        get_crypto_keys(key_ring_id)
+        get_key_rings()
+        initialize_cryptokey(crypto_key_id)
+        initialize_keyring(key_ring_id)
     """
 
     def __init__(self, tfvars_parser):
-        """Tfvars_Encryptor_GCP class constructor to initialize the object.
-        
+        """GCP_Tfvars_Encryptor class constructor to initialize the object.
+
         Args
         ----
         tfvars_parser : object 
-            Instance of a Tfvars_Parser class.
+            Instance of Tfvars_Parser class.
         """
 
         super().__init__(tfvars_parser)
@@ -425,16 +430,16 @@ class GCP_Tfvars_Encryptor(Tfvars_Encryptor):
 
     def create_crypto_key(self, crypto_key_id):
         """A method to create a crypto key on GCP KMS.
-        
+
         Args
         ----
         crypto_key_id : str
-            name of the crypto key to be created
+            name of the crypto key to be created.
 
         Returns
         -------
         response.name : str 
-            name of the crypto key created
+            name of the crypto key created.
         """
 
          # Create the crypto key object template
@@ -453,16 +458,16 @@ class GCP_Tfvars_Encryptor(Tfvars_Encryptor):
 
         Uses GCP KMS to decrypt ciphertext back to plaintext using the provided
         symmetric crypto key that belongs to this instance.
-        
+
         Args
         ----
         ciphertext : str
-            the ciphertext being decrypted
+            the ciphertext being decrypted.
 
         Returns
         -------
         plaintext : str
-            the decrypted secret in plaintext
+            the decrypted secret in plaintext.
         """
 
         # Convert ciphertext string to a byte string, then Base64 decode it
@@ -482,16 +487,16 @@ class GCP_Tfvars_Encryptor(Tfvars_Encryptor):
 
         Uses GCP KMS to encrypt plaintext to ciphertext using the provided
         symmetric crypto key that belongs to this instance.
-        
+
         Args
         ----
         ciphertext : str 
-            the plainttext being encrypted
+            the plainttext being encrypted.
 
         Returns
         -------
         ciphertext : str
-            the encrypted secret in ciphertext
+            the encrypted secret in ciphertext.
         """
 
         # Use the KMS API to encrypt the data.
@@ -507,11 +512,11 @@ class GCP_Tfvars_Encryptor(Tfvars_Encryptor):
         """A method that retrieves a list of crypto keys associated with a key ring.
 
         This method returns a list of all the crypto keys associated with a specific key ring.
-        
+
         Args
         ----
         key_ring_id : str
-            String ID for the GCP KMS key ring
+            string ID for the GCP KMS key ring.
 
         Returns
         -------
@@ -534,11 +539,11 @@ class GCP_Tfvars_Encryptor(Tfvars_Encryptor):
 
         This method returns a list of all the key rings associated 
         with the GCP service account.
-        
+
         Returns
         -------
         key_rings_list : list 
-            a list of all the key rings
+            a list of all the key rings.
         """
 
         parent   = self.kms_client.location_path(self.project_id, self.location)
@@ -557,16 +562,16 @@ class GCP_Tfvars_Encryptor(Tfvars_Encryptor):
         This initialization method is called in the constructor to
         create a default crypto key if it doesn't exist. If the key
         exists already, then reuse it for this instance.
-        
+
         Args
         ----
         crypto_key_id : str 
-            crypto key used to encrypt and decryp
+            the GCP crypto key ID used to encrypt and decrypt.
 
         Returns
         -------
         crypto_key_id : str
-            the crypto key used
+            the GCP crypto key ID used to encrypt and decrypt.
         """
 
         crypto_keys_list = self.get_crypto_keys(self.key_ring_id)
@@ -592,16 +597,16 @@ class GCP_Tfvars_Encryptor(Tfvars_Encryptor):
 
         This initialization method is called in the constructor to
         create a default key ring if it doesn't exist.
-        
+
         Args
         ----
         key_ring_id : str 
-            key ring being created
+            key ring being created.
 
         Returns
         -------
         key_ring_id : str 
-            the key ring used
+            the key ring used.
         """
 
         key_rings_list = self.get_key_rings()
@@ -611,7 +616,7 @@ class GCP_Tfvars_Encryptor(Tfvars_Encryptor):
             try:
                 self.create_key_ring(key_ring_id)
                 print("Created key ring: {}\n".format(key_ring_id))
-        
+
             except Exception as err:
                 print("An exception occurred creating new key ring:")
                 print("{}".format(err))
@@ -624,20 +629,20 @@ class GCP_Tfvars_Encryptor(Tfvars_Encryptor):
 
 class AWS_Tfvars_Encryptor(Tfvars_Encryptor):
     """This is a concrete sub class that inherits from Tfvars_Encryptor.
-    
+
     It contains attributes and methods specific to AWS KMS client to 
     automate the encryption and decryption of terraform.tfvars files.
 
     Attributes
     ----------
     aws_credentials : dict
-        Dictionary containing two keys, aws_access_key_id and aws_secret_access_key
+        Dictionary containing two keys: aws_access_key_id and aws_secret_access_key.
     customer_master_key_id : str 
         Defaulted to use "cas_key" as a crypto key ID.
 
     Methods
     -------
-        __init__(self, tfvars_parser)
+        __init__(tfvars_parser)
         create_crypto_key(crypto_key_alias)
         decrypt_ciphertext(ciphertext)
         encrypt_plaintext(plaintext)
@@ -648,11 +653,11 @@ class AWS_Tfvars_Encryptor(Tfvars_Encryptor):
 
     def __init__(self, tfvars_parser):
         """AWS_Tfvars_Encryptor class constructor to initialize the object.
-        
+
         Args
         ----
         tfvars_parser : object 
-            Instance of a Tfvars_Parser class.
+            instance of Tfvars_Parser class.
         """
 
         super().__init__(tfvars_parser)
@@ -671,12 +676,18 @@ class AWS_Tfvars_Encryptor(Tfvars_Encryptor):
 
     def create_crypto_key(self, crypto_key_alias):
         """A method to create a crypto key on AWS KMS.
-        
-        Args:
-            crypto_key_alias (str): the alias name of the crypto key to be created
-        Returns:
-            string: customer_master_key_id used for the tfvars
+
+        Args
+        ----
+        crypto_key_alias : str 
+            alias name of the crypto key being created.
+
+        Returns
+        ------
+        customer_master_key_id : string
+            customer_master_key_id value used for terraform.tfvars file.
         """
+
         # Use KMS client to create key and store the returned KeyId
         customer_master_key_id = self.kms_client.create_key().get('KeyMetadata').get('KeyId')
         
@@ -697,11 +708,16 @@ class AWS_Tfvars_Encryptor(Tfvars_Encryptor):
 
         Uses AWS KMS to decrypt ciphertext back to plaintext using the provided
         symmetric crypto key that belongs to this instance.
-        
-        Args:
-            ciphertext (str): the ciphertext being decrypted
-        Returns:
-            string: the plaintext
+
+        Args
+        ----
+        ciphertext : str
+            the ciphertext being decrypted.
+
+        Returns
+        -------
+        plaintext : str
+            the decrypted secret in plaintext.
         """
 
         # Convert ciphertext string to a byte string, then Base64 decode it
@@ -724,11 +740,16 @@ class AWS_Tfvars_Encryptor(Tfvars_Encryptor):
 
         Uses AWS KMS to encrypt plaintext to ciphertext using the provided
         symmetric crypto key that belongs to this instance.
-        
-        Args:
-            plaintext (str): the plainttext being encrypted
-        Returns:
-            string: the ciphertext
+
+        Args
+        ----
+        ciphertext : str 
+            the plainttext being encrypted.
+
+        Returns
+        -------
+        ciphertext : str
+            the encrypted secret in ciphertext.
         """
 
         # Use the KMS API to encrypt the data.
@@ -744,12 +765,13 @@ class AWS_Tfvars_Encryptor(Tfvars_Encryptor):
 
 
     def get_crypto_keys(self):
-        """A method that retrieves a list of crypto keys aliase names associated with the AWS credentials in the region.
+        """A method that retrieves a list of crypto keys aliase names 
+        associated with the AWS credentials in the region.
 
-        This method returns a list of all the crypto keys aliase names associated with the AWS credentials in the region.
-        
-        Returns:
-            list: a list of all the crypto keys aliase names associated with the AWS credentials in the region.
+        Returns
+        -------
+        crypto_keys_list : list
+            a list of all the crypto keys aliase names associated with the AWS credentials in the region.
         """
 
         # Use crypto keys data under the 'Aliases' dict key
@@ -769,11 +791,16 @@ class AWS_Tfvars_Encryptor(Tfvars_Encryptor):
         This initialization method is used in the constructor to
         initialize both the aws_access_key_id and aws_secret_access_key 
         by parsing the aws_credentials_file. 
-        
-        Args:
-            path (str): path to aws_credentials_file
-        Returns:
-            dict: Dictionary containing the 'aws_access_key_id' and 'aws_secret_access_key'.
+
+        Args
+        ----
+        path : str 
+            path to aws_credentials_file.
+
+        Returns
+        -------
+        dict
+            Dictionary containing the 'aws_access_key_id' and 'aws_secret_access_key'.
         """
 
         aws_access_key_id = None
@@ -795,7 +822,7 @@ class AWS_Tfvars_Encryptor(Tfvars_Encryptor):
                 if 'aws_access_key_id' in line:
                     aws_access_key_id = line.rpartition('=')[2].strip()
                     continue
-        
+
         return {    'aws_access_key_id': aws_access_key_id, 
                 'aws_secret_access_key': aws_secret_access_key }
 
@@ -806,11 +833,16 @@ class AWS_Tfvars_Encryptor(Tfvars_Encryptor):
         This initialization method is called in the constructor to
         create a default crypto key if it doesn't exist. If the key
         exists already, then reuses it for this instance.
-        
-        Args:
-            crypto_key_alias_name (str): crypto key used to encrypt and decrypt
-        Returns:
-            string: the crypto key used
+
+        Args
+        ----
+        crypto_key_alias_name : str
+            the AWS crypto key alias name used to encrypt and decrypt.
+
+        Returns
+        -------
+        customer_master_key_id : str 
+            the AWS crypto key used to encrypt and decrypt.
         """
 
         crypto_keys_list = self.get_crypto_keys()
