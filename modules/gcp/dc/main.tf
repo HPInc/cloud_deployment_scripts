b331ffa3be34e550ddf575cd4eb4fdc781ffe22a
Sherman Yin <syin@teradici.com>
2020-05-11 12:19:50 -0700

    Fix domain_users_list reference
    The code in Domain Controller module does not actually use the
    domain_users_list variable.
    
    Signed-off-by: Sherman Yin <syin@teradici.com>

--- modules/gcp/dc/main.tf
+++ modules/gcp/dc/main.tf
@@ -153,7 +153,7 @@ resource "null_resource" "upload-domain-users-list" {
   }
 
   provisioner "file" {
-    source      = "domain_users_list.csv"
+    source      = var.domain_users_list
     destination = local.domain_users_list_file
   }
 }
