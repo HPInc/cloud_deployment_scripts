a91e8daeddab79274676fd1fad23f4fbd28b10ee
Edwin-Pau <epau@teradici.com>
2020-06-12 11:58:08 -0600

    GCP: Fix Terraform install script
    The install script for Terraform has been optimized by removing
    unnecessary global variables and it now simply installs Terraform
    into the user specified directory from the command line argument.
    
    The logic to build the path to install Terraform in was redundant
    since the GCP quickstart script already handles this.
    
    Signed-off-by: Edwin-Pau <epau@teradici.com>

--- quickstart/install-terraform.py
+++ quickstart/install-terraform.py
@@ -14,12 +14,9 @@ import zipfile
 
 TEMP_DIR           = '/tmp'
 TERRAFORM_VERSION  = '0.12.3'
-HOME               = os.path.expanduser('~')
-TERRAFORM_BIN_DIR  = f'{HOME}/bin'
-TERRAFORM_BIN_PATH = TERRAFORM_BIN_DIR + '/terraform'
 
 
-def terraform_install(version=TERRAFORM_VERSION):
+def terraform_install(terraform_bin_dir, version=TERRAFORM_VERSION):
     zip_filename = 'terraform_{}_linux_amd64.zip'.format(version)
     download_url = 'https://releases.hashicorp.com/terraform/{}/{}'.format(version, zip_filename)
     local_zip_file = TEMP_DIR + '/' + zip_filename
@@ -27,12 +24,12 @@ def terraform_install(version=TERRAFORM_VERSION):
     print('Downloading from {} to {}...'.format(download_url, local_zip_file))
     urllib.request.urlretrieve(download_url, local_zip_file)
 
-    print('Extracting to {}...'.format(TERRAFORM_BIN_DIR))
-    pathlib.Path(TERRAFORM_BIN_DIR).mkdir(parents=True, exist_ok=True)
+    print('Extracting to {}...'.format(terraform_bin_dir))
+    pathlib.Path(terraform_bin_dir).mkdir(parents=True, exist_ok=True)
     with zipfile.ZipFile(local_zip_file) as tf_zip_file:
-        tf_zip_file.extractall(TERRAFORM_BIN_DIR)
+        tf_zip_file.extractall(terraform_bin_dir)
 
-    os.chmod(TERRAFORM_BIN_PATH, 0o775)
+    os.chmod(terraform_bin_dir + '/terraform', 0o775)
 
     print('Deleting {}...'.format(local_zip_file))
     os.remove(local_zip_file)
@@ -48,12 +45,10 @@ if __name__ == '__main__':
     parser.add_argument('dir', help='specify the directory to install Terraform')
     args = parser.parse_args()
 
-    TERRAFORM_BIN_DIR = args.dir
-
     # Don't attempt to install unless needed
     path = shutil.which('terraform')
     if path:
         print('Terraform already installed in ' + path)
         sys.exit(1)
 
-    terraform_install()
+    terraform_install(args.dir)
