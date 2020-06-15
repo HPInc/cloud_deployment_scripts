78c7577568051491e6b4f4732cade93a6b090c08
bsalla <bsalla@primesoft.net>
2020-06-15 15:16:46 +0530

    AWS: updated windows AMI to 2020.06.10

--- deployments/aws/single-connector/vars.tf
+++ deployments/aws/single-connector/vars.tf
@@ -79,7 +79,7 @@ variable "dc_ami_owner" {
 
 variable "dc_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2020.05.13"
+  default     = "Windows_Server-2019-English-Full-Base-2020.06.10"
 }
 
 variable "domain_name" {
@@ -227,7 +227,7 @@ variable "win_gfx_ami_owner" {
 
 variable "win_gfx_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2020.05.13"
+  default     = "Windows_Server-2019-English-Full-Base-2020.06.10"
 }
 
 variable "win_std_instance_count" {
@@ -257,7 +257,7 @@ variable "win_std_ami_owner" {
 
 variable "win_std_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2020.05.13"
+  default     = "Windows_Server-2019-English-Full-Base-2020.06.10"
 }
 
 variable "centos_gfx_instance_count" {
