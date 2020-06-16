37c2ce7b4c21099ceddb11df63e0c4acd8da8722
bsalla <bsalla@primesoft.net>
2020-06-16 17:12:44 +0530

    GCP: updated windowsOS in vars.tf and .tfvars files

--- deployments/gcp/multi-region/vars.tf
+++ deployments/gcp/multi-region/vars.tf
@@ -79,7 +79,7 @@ variable "dc_disk_size_gb" {
 
 variable "dc_disk_image" {
   description = "Disk image for the Domain Controller"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20200512"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20200609"
 }
 
 variable "dc_admin_password" {
@@ -271,7 +271,7 @@ variable "win_gfx_disk_size_gb" {
 
 variable "win_gfx_disk_image" {
   description = "Disk image for the Windows Graphics Workstation"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20200512"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20200609"
 }
 
 variable "win_std_instance_count_list" {
@@ -296,7 +296,7 @@ variable "win_std_disk_size_gb" {
 
 variable "win_std_disk_image" {
   description = "Disk image for the Windows Standard Workstation"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20200512"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20200609"
 }
 
 variable "centos_gfx_instance_count_list" {
