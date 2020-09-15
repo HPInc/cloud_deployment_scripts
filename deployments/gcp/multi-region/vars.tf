2fc578d6e25a258c437639896b83f91c332fcc19
bsalla <bsalla@primesoft.net>
2020-09-15 12:10:06 +0000

    GCP: updated Centos in var.tf and .tfvars files

--- deployments/gcp/multi-region/vars.tf
+++ deployments/gcp/multi-region/vars.tf
@@ -331,7 +331,7 @@ variable "centos_gfx_disk_size_gb" {
 
 variable "centos_gfx_disk_image" {
   description = "Disk image for the CentOS Graphics Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20200902"
+  default     = "projects/centos-cloud/global/images/centos-7-v20200910"
 }
 
 variable "centos_std_instance_count_list" {
@@ -356,7 +356,7 @@ variable "centos_std_disk_size_gb" {
 
 variable "centos_std_disk_image" {
   description = "Disk image for the CentOS Standard Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20200902"
+  default     = "projects/centos-cloud/global/images/centos-7-v20200910"
 }
 
 variable "centos_admin_user" {
