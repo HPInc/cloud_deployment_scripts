707c9663a0b3d41e8967f88e3740ccf06e9edcc4
Sherman Yin <syin@teradici.com>
2020-11-18 16:18:09 -0800

    GCP: clone single-connector->cam-single-connector
    Starting point of a CAM Standalone-based single-connector deployment
    
    Signed-off-by: Sherman Yin <syin@teradici.com>

---
+++ deployments/gcp/cam-single-connector/providers.tf
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 Teradici Corporation
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+provider "google" {
+  credentials = file(var.gcp_credentials_file)
+  project     = var.gcp_project_id
+  region      = var.gcp_region
+  zone        = var.gcp_zone
+}
+
+provider "google-beta" {
+  credentials = file(var.gcp_credentials_file)
+  project     = var.gcp_project_id
+  region      = var.gcp_region
+  zone        = var.gcp_zone
+}
