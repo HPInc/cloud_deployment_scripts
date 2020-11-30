99eb76b7839719362e2eae67477b2963282d4d14
Maggie Qiao <mqiao@teradici.com>
2020-11-29 22:35:28 -0800

    AWS: updated to latest required provider
    New required provider format for terraform 0.13 using "terraform
    0.13upgrade" command, using latest version for aws
    
    Signed-off-by: Maggie Qiao <mqiao@teradici.com>

--- deployments/aws/single-connector/versions.tf
+++ deployments/aws/single-connector/versions.tf
@@ -6,5 +6,17 @@
  */
 
 terraform {
-  required_version = ">= 0.13.5"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.18.0"
+    }
+    http = {
+      source = "hashicorp/http"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
 }
