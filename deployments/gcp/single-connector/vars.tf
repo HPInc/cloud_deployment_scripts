2cf1194bb9a88d563c3ed7f6564fbc09ccc0d766
bsalla <bsalla@primesoft.net>
2021-01-08 12:50:28 +0000

    GCP: Updated centos in vars.tf and tfvars.sample files

--- deployments/gcp/single-connector/vars.tf
+++ deployments/gcp/single-connector/vars.tf
@@ -291,7 +291,7 @@ variable "centos_gfx_disk_size_gb" {
 
 variable "centos_gfx_disk_image" {
   description = "Disk image for the CentOS Graphics Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20201112"
+  default     = "projects/centos-cloud/global/images/centos-7-v20201216"
 }
 
 variable "centos_std_instance_count" {
@@ -316,7 +316,7 @@ variable "centos_std_disk_size_gb" {
 
 variable "centos_std_disk_image" {
   description = "Disk image for the CentOS Standard Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20201112"
+  default     = "projects/centos-cloud/global/images/centos-7-v20201216"
 }
 
 variable "centos_admin_user" {
