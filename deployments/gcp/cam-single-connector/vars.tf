a61c87c32c922be846de9340cd0634b27024c07f
bsalla <bsalla@primesoft.net>
2021-01-12 13:20:43 +0000

    GCP: Updated Ubuntu OS in vars.tf files

--- deployments/gcp/cam-single-connector/vars.tf
+++ deployments/gcp/cam-single-connector/vars.tf
@@ -149,7 +149,7 @@ variable "cac_disk_size_gb" {
 
 variable "cac_disk_image" {
   description = "Disk image for the Cloud Access Connector"
-  default     = "projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20210105"
+  default     = "projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20210108"
 }
 
 # TODO: does this have to match the tag at the end of the SSH pub key?
