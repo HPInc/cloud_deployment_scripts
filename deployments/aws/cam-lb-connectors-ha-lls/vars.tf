7da90d5f4dfb1c849e878857d25bc2739358171e
bsalla <bsalla@primesoft.net>
2021-01-20 16:59:40 +0530

    AWS: Ubuntu OS update in vars.tf files

--- deployments/aws/cam-lb-connectors-ha-lls/vars.tf
+++ deployments/aws/cam-lb-connectors-ha-lls/vars.tf
@@ -272,7 +272,7 @@ variable "cac_ami_owner" {
 
 variable "cac_ami_name" {
   description = "Name of the AMI to create Cloud Access Connector from"
-  default = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20201211.1"
+  default = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20210112"
 }
 
 # Note the following limits for health check:
