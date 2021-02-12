e6d7c1c0b32eda30f53c88a643808ce391b5b347
hchitrachedu <hchitrachedu@teradici.com>
2021-02-12 11:51:28 +0000

    GCP: updated windows os in vars.tf and tfvars.sample file

--- deployments/gcp/single-connector/vars.tf
+++ deployments/gcp/single-connector/vars.tf
@@ -74,7 +74,7 @@ variable "dc_disk_size_gb" {
 
 variable "dc_disk_image" {
   description = "Disk image for the Domain Controller"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20210112"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20210209"
 }
 
 variable "dc_admin_password" {
@@ -261,7 +261,7 @@ variable "win_gfx_disk_size_gb" {
 
 variable "win_gfx_disk_image" {
   description = "Disk image for the Windows Graphics Workstation"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20210112"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20210209"
 }
 
 variable "win_std_instance_count" {
@@ -286,7 +286,7 @@ variable "win_std_disk_size_gb" {
 
 variable "win_std_disk_image" {
   description = "Disk image for the Windows Standard Workstation"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20210112"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20210209"
 }
 
 variable "centos_gfx_instance_count" {
