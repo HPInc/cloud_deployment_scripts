7ed0d7f8998b511cf6388b87336a07d572372d4f
Nathania Hendradjaja <nhendradjaja@teradici.com>
2021-03-09 16:37:40 -0800

    Domain Name Variable Validation For Modules
    Apply validation at module level to be consistent with how other
    variable validations are done.
    
    Signed-off-by: Nathania Hendradjaja <nhendradjaja@teradici.com>

--- modules/gcp/win-std/vars.tf
+++ modules/gcp/win-std/vars.tf
@@ -28,6 +28,20 @@ variable "pcoip_registration_code" {
 variable "domain_name" {
   description = "Name of the domain to join"
   type        = string
+
+  /* validation notes:
+      - the name is at least 2 levels and at most 3, as we have only tested up to 3 levels
+  */
+  validation {
+    condition = (
+      length(regexall("([.]local$)",var.domain_name)) == 0 &&
+      length(var.domain_name) < 256 &&
+      can(regex(
+        "(^[A-Za-z0-9][A-Za-z0-9-]{0,13}[A-Za-z0-9][.])([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9][.]){0,1}([A-Za-z]{2,}$)", 
+        var.domain_name))
+    )
+    error_message = "Domain name is invalid. Please try again."
+  }
 }
 
 variable "ad_service_account_username" {
