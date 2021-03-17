93417e7d0c93cc7fb80048568454a2e1e72b9e78
hchitrachedu <hchitrachedu@teradici.com>
2021-03-17 11:17:35 +0000

    GCP: Updated centos in vars.tf and tfvars.sample files

--- deployments/gcp/cas-mgr-single-connector/vars.tf
+++ deployments/gcp/cas-mgr-single-connector/vars.tf
@@ -400,7 +400,7 @@ variable "centos_gfx_disk_size_gb" {
 
 variable "centos_gfx_disk_image" {
   description = "Disk image for the CentOS Graphics Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20210217"
+  default     = "projects/centos-cloud/global/images/centos-7-v20210316"
 }
 
 variable "centos_std_instance_count" {
@@ -425,7 +425,7 @@ variable "centos_std_disk_size_gb" {
 
 variable "centos_std_disk_image" {
   description = "Disk image for the CentOS Standard Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20210217"
+  default     = "projects/centos-cloud/global/images/centos-7-v20210316"
 }
 
 variable "centos_admin_user" {
--- deployments/gcp/single-connector/vars.tf
+++ deployments/gcp/single-connector/vars.tf
@@ -360,7 +360,7 @@ variable "centos_gfx_disk_size_gb" {
 
 variable "centos_gfx_disk_image" {
   description = "Disk image for the CentOS Graphics Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20210217"
+  default     = "projects/centos-cloud/global/images/centos-7-v20210316"
 }
 
 variable "centos_std_instance_count" {
@@ -385,7 +385,7 @@ variable "centos_std_disk_size_gb" {
 
 variable "centos_std_disk_image" {
   description = "Disk image for the CentOS Standard Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20210217"
+  default     = "projects/centos-cloud/global/images/centos-7-v20210316"
 }
 
 variable "centos_admin_user" {
