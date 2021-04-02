f0540bbdd436965b755cfae573708e2ea6e4062b
Harish Chitrachedu <hchitrachedu@teradici.com>
2021-04-02 17:18:01 +0530

    AWS: Ubuntu OS update in vars.tf files

--- deployments/aws/lb-connectors/vars.tf
+++ deployments/aws/lb-connectors/vars.tf
@@ -174,7 +174,7 @@ variable "cac_ami_owner" {
 
 variable "cac_ami_name" {
   description = "Name of the AMI to create Cloud Access Connector from"
-  default = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20210315.1"
+  default = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20210325"
 }
 
 variable "cac_version" {
