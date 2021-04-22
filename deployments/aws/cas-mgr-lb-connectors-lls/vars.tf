34011928323aba0b1efabeb19675946952d00b0b
Harish Chitrachedu <hchitrachedu@teradici.com>
2021-04-22 17:47:19 +0530

    AWS : Updated Windowos in vars.tf and terraform.tfvars files

--- deployments/aws/cas-mgr-lb-connectors-lls/vars.tf
+++ deployments/aws/cas-mgr-lb-connectors-lls/vars.tf
@@ -99,7 +99,7 @@ variable "dc_ami_owner" {
 
 variable "dc_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2021.03.10"
+  default     = "Windows_Server-2019-English-Full-Base-2021.04.14"
 }
 
 variable "domain_name" {
@@ -390,7 +390,7 @@ variable "win_gfx_ami_owner" {
 
 variable "win_gfx_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2021.03.10"
+  default     = "Windows_Server-2019-English-Full-Base-2021.04.14"
 }
 
 variable "win_gfx_pcoip_agent_version" {
@@ -425,7 +425,7 @@ variable "win_std_ami_owner" {
 
 variable "win_std_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2021.03.10"
+  default     = "Windows_Server-2019-English-Full-Base-2021.04.14"
 }
 
 variable "win_std_pcoip_agent_version" {
