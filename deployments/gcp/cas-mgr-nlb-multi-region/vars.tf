f979112fa5295f606eee1825bbb40e2d2e55ecf5
Harish Chitrachedu <hchitrachedu@teradici.com>
2021-06-21 19:05:49 +0530

    GCP: Updated centos in vars.tf and tfvars.sample files

--- deployments/gcp/cas-mgr-nlb-multi-region/vars.tf
+++ deployments/gcp/cas-mgr-nlb-multi-region/vars.tf
@@ -420,7 +420,7 @@ variable "centos_gfx_disk_size_gb" {
 
 variable "centos_gfx_disk_image" {
   description = "Disk image for the CentOS Graphics Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20210512"
+  default     = "projects/centos-cloud/global/images/centos-7-v20210609"
 }
 
 variable "centos_std_instance_count_list" {
@@ -445,7 +445,7 @@ variable "centos_std_disk_size_gb" {
 
 variable "centos_std_disk_image" {
   description = "Disk image for the CentOS Standard Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20210512"
+  default     = "projects/centos-cloud/global/images/centos-7-v20210609"
 }
 
 variable "centos_admin_user" {
