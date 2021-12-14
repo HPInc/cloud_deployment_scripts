3893356268735c07a0bf7051d6197fba5a7bb0be
CloudCity Minion <minion@cloudcity.teradici.com>
2021-12-14 13:02:42 +0000

    Updated OS versions from cloud provider
    Updated disk images to latest versions from gcp
    
    Signed-off-by: CloudCity Minion <minion@cloudcity.teradici.com>

--- deployments/gcp/single-connector/vars.tf
+++ deployments/gcp/single-connector/vars.tf
@@ -375,7 +375,7 @@ variable "centos_gfx_disk_size_gb" {
 
 variable "centos_gfx_disk_image" {
   description = "Disk image for the CentOS Graphics Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20211105"
+  default     = "projects/centos-cloud/global/images/centos-7-v20211209"
 }
 
 variable "centos_std_instance_count" {
@@ -400,7 +400,7 @@ variable "centos_std_disk_size_gb" {
 
 variable "centos_std_disk_image" {
   description = "Disk image for the CentOS Standard Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20211105"
+  default     = "projects/centos-cloud/global/images/centos-7-v20211209"
 }
 
 variable "centos_admin_user" {
