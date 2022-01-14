a4f4e8792b7890a7b0443bc3338fcb2aa08fb0da
CloudCity Minion <minion@cloudcity.teradici.com>
2022-01-14 13:03:14 +0000

    Updated OS versions from cloud provider
    Updated disk images to latest versions from aws
    
    Windows_Server-2019-English-Full-Base-2022.01.12
    
    Signed-off-by: CloudCity Minion <minion@cloudcity.teradici.com>

--- deployments/aws/lb-connectors-ha-lls/vars.tf
+++ deployments/aws/lb-connectors-ha-lls/vars.tf
@@ -79,7 +79,7 @@ variable "dc_ami_owner" {
 
 variable "dc_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2021.12.15"
+  default     = "Windows_Server-2019-English-Full-Base-2022.01.12"
 }
 
 variable "dc_pcoip_agent_version" {
@@ -388,7 +388,7 @@ variable "win_gfx_ami_owner" {
 
 variable "win_gfx_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2021.12.15"
+  default     = "Windows_Server-2019-English-Full-Base-2022.01.12"
 }
 
 variable "win_gfx_pcoip_agent_version" {
@@ -428,7 +428,7 @@ variable "win_std_ami_owner" {
 
 variable "win_std_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2021.12.15"
+  default     = "Windows_Server-2019-English-Full-Base-2022.01.12"
 }
 
 variable "centos_gfx_instance_count" {
