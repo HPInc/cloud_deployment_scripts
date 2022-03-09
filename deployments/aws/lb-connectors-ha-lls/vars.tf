ba677af81bcecb8fff410b3407933899fab819f4
Daniel Bergel <daniel.bergel@hp.com>
2022-03-09 13:03:02 +0000

    AWS: Updated OS images: Ubuntu
    ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20220308
    
    Signed-off-by: Daniel Bergel <daniel.bergel@hp.com>

--- deployments/aws/lb-connectors-ha-lls/vars.tf
+++ deployments/aws/lb-connectors-ha-lls/vars.tf
@@ -256,7 +256,7 @@ variable "cac_ami_owner" {
 
 variable "cac_ami_name" {
   description = "Name of the AMI to create Cloud Access Connector from"
-  default = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20220302"
+  default = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20220308"
 }
 
 variable "cac_version" {
