2e1dacb10cb9a152a1eff75bbb3934229914682b
Qi Dang <qidang@hp.com>
2022-03-21 11:42:29 -0700

    AWS: to not upload cloudwatch setup scripts when cloudwatch is disabled
    We are no longer uploading cloudwatch setup scripts when cloudwatch is disabled.
    
    Signed-off-by: Qi Dang <qidang@hp.com>

--- deployments/aws/lb-connectors/main.tf
+++ deployments/aws/lb-connectors/main.tf
@@ -43,18 +43,24 @@ resource "aws_s3_bucket_object" "cas-mgr-deployment-sa-file" {
 }
 
 resource "aws_s3_bucket_object" "cloudwatch-setup-rpm-script" {
+  count = var.cloudwatch_enable ? 1 : 0
+
   bucket = aws_s3_bucket.scripts.id
   key    = local.cloudwatch_setup_rpm_script
   source = "../../../shared/aws/${local.cloudwatch_setup_rpm_script}"
 }
 
 resource "aws_s3_bucket_object" "cloudwatch-setup-deb-script" {
+  count = var.cloudwatch_enable ? 1 : 0
+
   bucket = aws_s3_bucket.scripts.id
   key    = local.cloudwatch_setup_deb_script
   source = "../../../shared/aws/${local.cloudwatch_setup_deb_script}"
 }
 
 resource "aws_s3_bucket_object" "cloudwatch-setup-win-script" {
+  count = var.cloudwatch_enable ? 1 : 0
+
   bucket = aws_s3_bucket.scripts.id
   key    = local.cloudwatch_setup_win_script
   source = "../../../shared/aws/${local.cloudwatch_setup_win_script}"
