4d13ad80fdb141bfda5d0fa7eac9644967dc4247
Daniel Bergel <daniel.bergel@hp.com>
2022-03-28 12:03:16 +0000

    GCP: Updated OS images: Ubuntu
    projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20220325
    
    Signed-off-by: Daniel Bergel <daniel.bergel@hp.com>

--- deployments/gcp/single-connector/vars.tf
+++ deployments/gcp/single-connector/vars.tf
@@ -115,7 +115,7 @@ variable "cac_disk_size_gb" {
 
 variable "cac_disk_image" {
   description = "Disk image for the Cloud Access Connector"
-  default     = "projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20220308"
+  default     = "projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20220325"
 }
 
 # TODO: does this have to match the tag at the end of the SSH pub key?
