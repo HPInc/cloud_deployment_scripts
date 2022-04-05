216cc09db7b3e81ae2c214dc8f358f108345adef
Qi Dang <qidang@hp.com>
2022-04-05 11:28:20 -0700

    Update copyright
    Signed-off-by: Qi Dang <qidang@hp.com>

--- deployments/aws/cas-mgr-single-connector/main.tf
+++ deployments/aws/cas-mgr-single-connector/main.tf
@@ -1,5 +1,5 @@
 /*
- * Copyright Teradici Corporation 2020-2022;  © Copyright 2022 HP Development Company, L.P.
+ * Copyright Teradici Corporation 2020-2021;  © Copyright 2021-2022 HP Development Company, L.P.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
