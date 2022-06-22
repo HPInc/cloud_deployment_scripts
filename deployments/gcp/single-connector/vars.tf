a2e9f7b8437f6813a5fd625cf67fb664dab12708
Daniel Bergel <daniel.bergel@hp.com>
2022-06-22 12:03:31 +0000

    GCP: Updated OS images: CentOS 7, Rocky 8, Ubuntu
    projects/centos-cloud/global/images/centos-7-v20220621
    projects/rocky-linux-cloud/global/images/rocky-linux-8-v20220621
    projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20220616
    
    Signed-off-by: Daniel Bergel <daniel.bergel@hp.com>

--- deployments/gcp/single-connector/vars.tf
+++ deployments/gcp/single-connector/vars.tf
@@ -122,7 +122,7 @@ variable "cac_disk_size_gb" {
 
 variable "cac_disk_image" {
   description = "Disk image for the Cloud Access Connector"
-  default     = "projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20220610"
+  default     = "projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20220616"
 }
 
 # TODO: does this have to match the tag at the end of the SSH pub key?
@@ -407,7 +407,7 @@ variable "centos_gfx_disk_size_gb" {
 
 variable "centos_gfx_disk_image" {
   description = "Disk image for the CentOS Graphics Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20220519"
+  default     = "projects/centos-cloud/global/images/centos-7-v20220621"
 }
 
 variable "centos_std_instance_count" {
@@ -432,7 +432,7 @@ variable "centos_std_disk_size_gb" {
 
 variable "centos_std_disk_image" {
   description = "Disk image for the CentOS Standard Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20220519"
+  default     = "projects/centos-cloud/global/images/centos-7-v20220621"
 }
 
 variable "centos_admin_user" {
