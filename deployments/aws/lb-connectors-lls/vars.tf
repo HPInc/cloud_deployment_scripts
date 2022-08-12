e21c612ff09e70b3ff60b2307f1c71376d40bbb2
Daniel Bergel <daniel.bergel@hp.com>
2022-08-12 12:03:39 +0000

    AWS: Updated OS images: Windows
    Windows_Server-2019-English-Full-Base-2022.08.10
    
    Signed-off-by: Daniel Bergel <daniel.bergel@hp.com>

--- deployments/aws/lb-connectors-lls/vars.tf
+++ deployments/aws/lb-connectors-lls/vars.tf
@@ -86,7 +86,7 @@ variable "dc_ami_owner" {
 
 variable "dc_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2022.07.13"
+  default     = "Windows_Server-2019-English-Full-Base-2022.08.10"
 }
 
 variable "dc_pcoip_agent_version" {
@@ -368,7 +368,7 @@ variable "win_gfx_ami_owner" {
 
 variable "win_gfx_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2022.07.13"
+  default     = "Windows_Server-2019-English-Full-Base-2022.08.10"
 }
 
 variable "win_gfx_pcoip_agent_version" {
@@ -403,7 +403,7 @@ variable "win_std_ami_owner" {
 
 variable "win_std_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2022.07.13"
+  default     = "Windows_Server-2019-English-Full-Base-2022.08.10"
 }
 
 variable "win_std_pcoip_agent_version" {
