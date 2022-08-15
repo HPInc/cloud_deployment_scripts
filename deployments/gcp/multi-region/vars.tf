05191c80c671a0e1eaeab036fb45e4d25400e4a3
Daniel Bergel <daniel.bergel@hp.com>
2022-08-15 12:03:46 +0000

    GCP: Updated OS images: Windows
    projects/windows-cloud/global/images/windows-server-2019-dc-v20220812
    
    Signed-off-by: Daniel Bergel <daniel.bergel@hp.com>

--- deployments/gcp/multi-region/vars.tf
+++ deployments/gcp/multi-region/vars.tf
@@ -81,7 +81,7 @@ variable "dc_disk_size_gb" {
 
 variable "dc_disk_image" {
   description = "Disk image for the Domain Controller"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20220712"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20220812"
 }
 
 variable "dc_admin_password" {
@@ -362,7 +362,7 @@ variable "win_gfx_disk_size_gb" {
 
 variable "win_gfx_disk_image" {
   description = "Disk image for the Windows Graphics Workstation"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20220712"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20220812"
 }
 
 variable "win_gfx_pcoip_agent_version" {
@@ -392,7 +392,7 @@ variable "win_std_disk_size_gb" {
 
 variable "win_std_disk_image" {
   description = "Disk image for the Windows Standard Workstation"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20220712"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20220812"
 }
 
 variable "win_std_pcoip_agent_version" {
