6e938b75314f507e7ff2eda9d97f5e45fd9f3eb3
Daniel Bergel <daniel.bergel@hp.com>
2023-01-16 13:03:15 +0000

    GCP: Updated OS images: Ubuntu, Windows
    projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20230112
    projects/windows-cloud/global/images/windows-server-2019-dc-v20230111
    
    Signed-off-by: Daniel Bergel <daniel.bergel@hp.com>

--- deployments/gcp/multi-region/vars.tf
+++ deployments/gcp/multi-region/vars.tf
@@ -81,7 +81,7 @@ variable "dc_disk_size_gb" {
 
 variable "dc_disk_image" {
   description = "Disk image for the Domain Controller"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20221214"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20230111"
 }
 
 variable "dc_admin_password" {
@@ -174,7 +174,7 @@ variable "cac_disk_size_gb" {
 
 variable "cac_disk_image" {
   description = "Disk image for the Cloud Access Connector"
-  default     = "projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20221201"
+  default     = "projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20230112"
 }
 
 # TODO: does this have to match the tag at the end of the SSH pub key?
@@ -364,7 +364,7 @@ variable "win_gfx_disk_size_gb" {
 
 variable "win_gfx_disk_image" {
   description = "Disk image for the Windows Graphics Workstation"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20221214"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20230111"
 }
 
 variable "win_gfx_pcoip_agent_version" {
@@ -394,7 +394,7 @@ variable "win_std_disk_size_gb" {
 
 variable "win_std_disk_image" {
   description = "Disk image for the Windows Standard Workstation"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20221214"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20230111"
 }
 
 variable "win_std_pcoip_agent_version" {
