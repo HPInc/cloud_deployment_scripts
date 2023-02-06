2b03a6b1c57660f065c674994bff8273156eb6b0
Daniel Bergel <daniel.bergel@hp.com>
2023-02-06 13:03:49 +0000

    GCP: Updated OS images: CentOS 7, Rocky 8
    projects/centos-cloud/global/images/centos-7-v20230203
    projects/rocky-linux-cloud/global/images/rocky-linux-8-v20230202
    
    Signed-off-by: Daniel Bergel <daniel.bergel@hp.com>

--- deployments/gcp/multi-region/vars.tf
+++ deployments/gcp/multi-region/vars.tf
@@ -439,7 +439,7 @@ variable "centos_gfx_disk_size_gb" {
 
 variable "centos_gfx_disk_image" {
   description = "Disk image for the CentOS Graphics Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20221206"
+  default     = "projects/centos-cloud/global/images/centos-7-v20230203"
 }
 
 variable "centos_std_instance_count_list" {
@@ -464,7 +464,7 @@ variable "centos_std_disk_size_gb" {
 
 variable "centos_std_disk_image" {
   description = "Disk image for the CentOS Standard Workstation"
-  default     = "projects/centos-cloud/global/images/centos-7-v20221206"
+  default     = "projects/centos-cloud/global/images/centos-7-v20230203"
 }
 
 variable "centos_admin_user" {
