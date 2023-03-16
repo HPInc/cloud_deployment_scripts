63c4a6bb25b39300abeb91ba35fa5ac9a343a9d1
Sofia Zhang <xiaoyang.zhang@hp.com>
2023-03-16 16:37:05 -0700

    AWS: Updated OS images: Windows
    Updates the dc, win-gfx, win-std ami to the latest version, synchronize from staging branch
    Windows_Server-2019-English-Full-Base-2023.02.15
    
    Signed-off-by: Sofia Zhang <xiaoyang.zhang@hp.com>

--- deployments/aws/single-connector/vars.tf
+++ deployments/aws/single-connector/vars.tf
@@ -86,7 +86,7 @@ variable "dc_ami_owner" {
 
 variable "dc_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2022.11.10"
+  default     = "Windows_Server-2019-English-Full-Base-2023.02.15"
 }
 
 variable "dc_admin_password" {
@@ -298,7 +298,7 @@ variable "win_gfx_ami_owner" {
 
 variable "win_gfx_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2022.11.10"
+  default     = "Windows_Server-2019-English-Full-Base-2023.02.15"
 }
 
 variable "win_gfx_pcoip_agent_version" {
@@ -333,7 +333,7 @@ variable "win_std_ami_owner" {
 
 variable "win_std_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2022.11.10"
+  default     = "Windows_Server-2019-English-Full-Base-2023.02.15"
 }
 
 variable "win_std_pcoip_agent_version" {
