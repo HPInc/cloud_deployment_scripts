228aac0d13a35ff9893e10775c3bfec1ed450f11
Daniel Bergel <daniel.bergel@hp.com>
2023-04-14 12:03:31 +0000

    AWS: Updated OS images: Windows
    Windows_Server-2019-English-Full-Base-2023.04.12
    
    Signed-off-by: Daniel Bergel <daniel.bergel@hp.com>

--- deployments/aws/lb-connectors-ha-lls/vars.tf
+++ deployments/aws/lb-connectors-ha-lls/vars.tf
@@ -86,7 +86,7 @@ variable "dc_ami_owner" {
 
 variable "dc_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2023.03.15"
+  default     = "Windows_Server-2019-English-Full-Base-2023.04.12"
 }
 
 variable "dc_pcoip_agent_install" {
@@ -402,7 +402,7 @@ variable "win_gfx_ami_owner" {
 
 variable "win_gfx_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2023.03.15"
+  default     = "Windows_Server-2019-English-Full-Base-2023.04.12"
 }
 
 variable "win_gfx_pcoip_agent_version" {
@@ -442,7 +442,7 @@ variable "win_std_ami_owner" {
 
 variable "win_std_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2023.03.15"
+  default     = "Windows_Server-2019-English-Full-Base-2023.04.12"
 }
 
 variable "centos_gfx_instance_count" {
--- deployments/aws/single-connector/vars.tf
+++ deployments/aws/single-connector/vars.tf
@@ -86,7 +86,7 @@ variable "dc_ami_owner" {
 
 variable "dc_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2023.03.15"
+  default     = "Windows_Server-2019-English-Full-Base-2023.04.12"
 }
 
 variable "dc_admin_password" {
@@ -308,7 +308,7 @@ variable "win_gfx_ami_owner" {
 
 variable "win_gfx_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2023.03.15"
+  default     = "Windows_Server-2019-English-Full-Base-2023.04.12"
 }
 
 variable "win_gfx_pcoip_agent_version" {
@@ -343,7 +343,7 @@ variable "win_std_ami_owner" {
 
 variable "win_std_ami_name" {
   description = "Name of the Windows AMI to create workstation from"
-  default     = "Windows_Server-2019-English-Full-Base-2023.03.15"
+  default     = "Windows_Server-2019-English-Full-Base-2023.04.12"
 }
 
 variable "win_std_pcoip_agent_version" {
