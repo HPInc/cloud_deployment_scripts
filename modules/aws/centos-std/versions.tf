fa99d69b0a870376d8b12be5432947eae79d80a5
Pvs <manogna.pvs@hp.com>
2023-06-16 20:22:14 +0000

    Updated the AWS Provider version to v5.x
    The code in versions.tf has been updated to utilize the latest versions
    of the AWS provider that are supported by Terraform. This change ensures
    that the code is compatible with the most recent features and
    improvements provided by the AWS provider in Terraform.
    
    Signed-off-by: Pvs <manogna.pvs@hp.com>

--- modules/aws/centos-std/versions.tf
+++ modules/aws/centos-std/versions.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.13.0"
+      version = "~> 5.0"
     }
     template = {
       source = "hashicorp/template"
