0eb1cf167ccc6b07cd93d6b12b8f31449087595e
Visal Saosuo <visal.saosuo@hp.com>
2024-02-15 17:32:16 -0800

    AWS: Create Rocky 8 standard and graphic modules
    This commit creates two modules(./modules/aws/rocky-std and
    ./modules/aws/rocky-gfx) that are based on CentOS modules configuration
    for Rocky 8. The modification ensures the correct naming convention
    and default value of the paremeters.
    
    Signed-off-by: Visal Saosuo <visal.saosuo@hp.com>

---
+++ modules/aws/rocky-gfx/main.tf
@@ -0,0 +1,293 @@
+/*
+ * Copyright Teradici Corporation 2020-2022;  © Copyright 2022-2024 HP Development Company, L.P.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+locals {
+  prefix = var.prefix != "" ? "${var.prefix}-" : ""
+
+  # Windows computer names must be <= 15 characters, minus 4 chars for "-xyz"
+  # where xyz is number of instances (0-999)
+  host_name = substr("${local.prefix}${var.instance_name}", 0, 11)
+
+  provisioning_script = "rocky-gfx-provisioning.sh"
+}
+
+resource "aws_s3_object" "rocky-gfx-provisioning-script" {
+  count = tonumber(var.instance_count) == 0 ? 0 : 1
+
+  key    = local.provisioning_script
+  bucket = var.bucket_name
+  content = templatefile(
+    "${path.module}/${local.provisioning_script}.tmpl",
+    {
+      ad_service_account_password_id = var.ad_service_account_password_id,
+      ad_service_account_username    = var.ad_service_account_username,
+      aws_region                     = var.aws_region,
+      aws_ssm_enable                 = var.aws_ssm_enable,
+      bucket_name                    = var.bucket_name,
+      cloudwatch_enable              = var.cloudwatch_enable,
+      cloudwatch_setup_script        = var.cloudwatch_setup_script,
+      domain_controller_ip           = var.domain_controller_ip,
+      domain_name                    = var.domain_name,
+      nvidia_driver_url              = var.nvidia_driver_url,
+      pcoip_registration_code_id     = var.pcoip_registration_code_id,
+      teradici_download_token        = var.teradici_download_token,
+
+      auto_logoff_cpu_utilization                = var.auto_logoff_cpu_utilization,
+      auto_logoff_enable                         = var.auto_logoff_enable,
+      auto_logoff_minutes_idle_before_logoff     = var.auto_logoff_minutes_idle_before_logoff,
+      auto_logoff_polling_interval_minutes       = var.auto_logoff_polling_interval_minutes,
+      idle_shutdown_cpu_utilization              = var.idle_shutdown_cpu_utilization,
+      idle_shutdown_enable                       = var.idle_shutdown_enable,
+      idle_shutdown_minutes_idle_before_shutdown = var.idle_shutdown_minutes_idle_before_shutdown,
+      idle_shutdown_polling_interval_minutes     = var.idle_shutdown_polling_interval_minutes,
+    }
+  )
+}
+
+data "template_file" "user-data" {
+  template = file("${path.module}/user-data.sh.tmpl")
+
+  vars = {
+    bucket_name         = var.bucket_name,
+    provisioning_script = local.provisioning_script,
+  }
+}
+
+# Need to do this to look up AMI ID, which is different for each region
+data "aws_ami" "ami" {
+  most_recent = true
+  owners      = [var.ami_owner]
+
+  filter {
+    name   = "name"
+    values = [var.ami_name]
+  }
+}
+
+data "aws_iam_policy_document" "instance-assume-role-policy-doc" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "rocky-gfx-role" {
+  count = tonumber(var.instance_count) == 0 ? 0 : 1
+
+  name               = "${local.prefix}rocky_gfx_role"
+  assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy-doc.json
+}
+
+data "aws_iam_policy_document" "rocky-gfx-policy-doc" {
+  statement {
+    actions   = ["ec2:DescribeTags"]
+    resources = ["*"]
+    effect    = "Allow"
+  }
+
+  statement {
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = [
+      "${var.pcoip_registration_code_id}",
+      "${var.ad_service_account_password_id}"
+    ]
+    effect    = "Allow"
+  }
+
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${var.bucket_name}/${local.provisioning_script}"]
+    effect    = "Allow"
+  }
+
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["arn:aws:s3:::${var.bucket_name}/${var.cloudwatch_setup_script}"]
+    effect    = "Allow"
+  }
+
+  statement {
+    actions = ["logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:DescribeLogStreams",
+    "logs:PutLogEvents"]
+    resources = ["arn:aws:logs:*:*:*"]
+    effect    = "Allow"
+  }
+
+  # add minimal permissions to allow users to connect to instances using Session Manager
+  dynamic "statement" {
+    for_each = var.aws_ssm_enable ? [1] : []
+    content {
+      actions = ["ssm:UpdateInstanceInformation",
+        "ssmmessages:CreateControlChannel",
+        "ssmmessages:CreateDataChannel",
+        "ssmmessages:OpenControlChannel",
+      "ssmmessages:OpenDataChannel"]
+      resources = ["*"]
+      effect    = "Allow"
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "rocky-gfx-role-policy" {
+  count = tonumber(var.instance_count) == 0 ? 0 : 1
+
+  name   = "${local.prefix}rocky_gfx_role_policy"
+  role   = aws_iam_role.rocky-gfx-role[0].id
+  policy = data.aws_iam_policy_document.rocky-gfx-policy-doc.json
+}
+
+resource "aws_iam_instance_profile" "rocky-gfx-instance-profile" {
+  count = tonumber(var.instance_count) == 0 ? 0 : 1
+
+  name = "${local.prefix}rocky_gfx_instance_profile"
+  role = aws_iam_role.rocky-gfx-role[0].name
+}
+
+resource "aws_cloudwatch_log_group" "instance-log-group" {
+  count = var.cloudwatch_enable ? var.instance_count : 0
+
+  name = "${local.host_name}-${count.index}"
+}
+
+resource "time_sleep" "delay_destroy_log_group" {
+  depends_on = [aws_cloudwatch_log_group.instance-log-group]
+
+  destroy_duration = "5s"
+}
+
+resource "aws_instance" "rocky-gfx" {
+  # wait 5 seconds before deleting the log group to account for delays in 
+  # Cloudwatch receiving the last messages before an EC2 instance is shut down
+  depends_on = [time_sleep.delay_destroy_log_group]
+
+  count = var.instance_count
+
+  ami           = data.aws_ami.ami.id
+  instance_type = var.instance_type
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = var.disk_size_gb
+  }
+
+  subnet_id                   = var.subnet
+  associate_public_ip_address = var.enable_public_ip
+
+  vpc_security_group_ids = var.security_group_ids
+
+  key_name = var.admin_ssh_key_name
+
+  iam_instance_profile = aws_iam_instance_profile.rocky-gfx-instance-profile[0].name
+
+  user_data = data.template_file.user-data.rendered
+
+  tags = {
+    Name = "${local.host_name}-${count.index}"
+  }
+}
+
+resource "aws_cloudwatch_dashboard" "main" {
+  count = var.cloudwatch_enable ? var.instance_count : 0
+
+  dashboard_name = "${local.host_name}-${count.index}"
+
+  dashboard_body = <<EOF
+{
+    "widgets": [
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 0,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "region": "${var.aws_region}",
+                "title": "CPU Utilization (%)",
+                "metrics": [
+                    [ "AWS/EC2", "CPUUtilization", "InstanceId", "${aws_instance.rocky-gfx[count.index].id}"]
+                ],
+                "view": "timeSeries",
+                "stat": "Average",
+                "period": 300,
+                "stacked": false
+            }
+        },
+        {
+            "type": "log",
+            "x": 12,
+            "y": 0,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "region": "${var.aws_region}",
+                "title": "Latency (ms)",
+                "query": "SOURCE '${local.host_name}-${count.index}' | filter @message like /round trip/ | parse @message 'Tx thread info: round trip time (ms) = *, variance' as rtt | stats pct(rtt, 50), pct(rtt, 80), pct(rtt, 90) by bin(10m)",
+                "view": "timeSeries"
+            }
+        }, 
+        {
+            "type": "log",
+            "x": 0,
+            "y": 6,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "region": "${var.aws_region}",
+                "title": "TxLoss (%)",
+                "query": "SOURCE '${local.host_name}-${count.index}' | filter @message like /Loss=/ | parse @message '(A/I/O) Loss=*%/*% (R/T)' as RxLoss, TxLoss | stats pct(TxLoss, 90), pct(TxLoss, 95) by bin(2m)",
+                "view": "timeSeries"
+            }
+        },
+        {
+            "type": "log",
+            "x": 12,
+            "y": 6,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "region": "${var.aws_region}",
+                "title": "RxLoss (%)",
+                "query": "SOURCE '${local.host_name}-${count.index}' | filter @message like /Loss=/ | parse @message '(A/I/O) Loss=*%/*% (R/T)' as RxLoss, TxLoss | stats pct(RxLoss, 90), pct(RxLoss, 95) by bin(2m)",
+                "view": "timeSeries"
+            }
+        },
+        {
+            "type": "log",
+            "x": 0,
+            "y": 12,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "region": "${var.aws_region}",
+                "title": "Data Transmitted (MiB)",
+                "query": "SOURCE '${local.host_name}-${count.index}' | filter @message like /MGMT_PCOIP_DATA :Tx thread info: bw limit/ | parse @message 'MGMT_PCOIP_DATA :Tx thread info: bw limit = *, avg tx = *, avg rx = * (kbit' as bwlimit, avgtx, avgrx | stats sum(avgtx)*60/8/1024 as Transmit_MiB by bin(10m)",
+                "view": "timeSeries"
+            }
+        },
+        {
+            "type": "log",
+            "x": 12,
+            "y": 12,
+            "width": 12,
+            "height": 6,
+            "properties": {
+                "region": "${var.aws_region}",
+                "title": "Data Reveived (MiB)",
+                "query": "SOURCE '${local.host_name}-${count.index}' | filter @message like /MGMT_PCOIP_DATA :Tx thread info: bw limit/ | parse @message 'MGMT_PCOIP_DATA :Tx thread info: bw limit = *, avg tx = *, avg rx = * (kbit' as bwlimit, avgtx, avgrx | stats sum(avgrx)*60/8/1024 as Reveive_MiB by bin(10m)",
+                "view": "timeSeries"
+            }
+        }
+    ]
+}
+EOF
+}
---
+++ modules/aws/rocky-std/vars.tf
@@ -0,0 +1,172 @@
+/*
+ * Copyright Teradici Corporation 2020-2022;  © Copyright 2022-2024 HP Development Company, L.P.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-west-1"
+}
+
+variable "prefix" {
+  description = "Prefix to add to name of new resources"
+  default     = ""
+}
+
+variable "instance_name" {
+  description = "Basename of hostname of the workstation. Hostname will be <prefix>-<name>-<number>. Lower case only."
+  default     = "srock"
+}
+
+variable "pcoip_registration_code_id" {
+  description = "Secrets Manager ARN for PCoIP Registration code"
+  type        = string
+  sensitive   = true
+}
+
+variable "domain_name" {
+  description = "Name of the domain to join"
+  type        = string
+
+  /* validation notes:
+      - the name is at least 2 levels and at most 3, as we have only tested up to 3 levels
+  */
+  validation {
+    condition = (
+      length(regexall("([.]local$)", var.domain_name)) == 0 &&
+      length(var.domain_name) < 256 &&
+      can(regex(
+        "(^[A-Za-z0-9][A-Za-z0-9-]{0,13}[A-Za-z0-9][.])([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9][.]){0,1}([A-Za-z]{2,}$)",
+      var.domain_name))
+    )
+    error_message = "Domain name is invalid. Please try again."
+  }
+}
+
+variable "domain_controller_ip" {
+  description = "Internal IP of the Domain Controller"
+  type        = string
+}
+
+variable "ad_service_account_username" {
+  description = "Active Directory Service Account username"
+  type        = string
+}
+
+variable "ad_service_account_password_id" {
+  description = "Secrets Manager ARN for Active Directory Service Account password"
+  type        = string
+  sensitive   = true
+}
+
+variable "bucket_name" {
+  description = "Name of bucket to retrieve provisioning script."
+  type        = string
+}
+
+variable "subnet" {
+  description = "Subnet to deploy the Workstation"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "Security Groups to be applied to the Workstation"
+  type        = list(string)
+}
+
+variable "enable_public_ip" {
+  description = "Assign a public IP to the workstation"
+  default     = false
+}
+
+variable "instance_count" {
+  description = "Number of Rocky Standard Workstations to deploy"
+  default     = 1
+}
+
+variable "instance_type" {
+  description = "Instance type for the Workstation"
+  default     = "t3.xlarge"
+}
+
+variable "disk_size_gb" {
+  description = "Disk size (GB) of the Workstation"
+  default     = "50"
+}
+
+variable "ami_owner" {
+  description = "Owner of AMI for the Workstation"
+  default     = "792107900819"
+}
+
+variable "ami_name" {
+  description = "Name of the AMI to create Workstation from"
+  default     = "Rocky-8-ec2*x86_64*"
+}
+
+variable "admin_ssh_key_name" {
+  description = "Name of Admin SSH Key"
+  type        = string
+}
+
+variable "teradici_download_token" {
+  description = "Token used to download from Teradici"
+  default     = "yj39yHtgj68Uv2Qf"
+}
+
+variable "auto_logoff_enable" {
+  description = "Enable auto log-off for Workstations"
+  default     = true
+}
+
+variable "auto_logoff_minutes_idle_before_logoff" {
+  description = "Minimum idle time for Workstations before auto log-off, must be between 5 and 10000"
+  default     = 20
+}
+
+variable "auto_logoff_polling_interval_minutes" {
+  description = "Polling interval for checking CPU utilization to determine if machine is idle, must be between 1 and 100"
+  default     = 5
+}
+
+variable "auto_logoff_cpu_utilization" {
+  description = "CPU utilization percentage, must be between 1 and 100"
+  default     = 20
+}
+
+variable "idle_shutdown_enable" {
+  description = "Enable Anyware Manager auto idle shutdown for Workstations"
+  default     = true
+}
+
+variable "idle_shutdown_minutes_idle_before_shutdown" {
+  description = "Minimum idle time for Workstations before auto idle shutdown, must be between 5 and 10000"
+  default     = 240
+}
+
+variable "idle_shutdown_polling_interval_minutes" {
+  description = "Polling interval for checking CPU utilization to determine if machine is idle, must be between 1 and 60"
+  default     = 15
+}
+
+variable "idle_shutdown_cpu_utilization" {
+  description = "CPU utilization percentage, must be between 1 and 100"
+  default     = 20
+}
+
+variable "cloudwatch_setup_script" {
+  description = "The script that sets up the AWS CloudWatch Logs agent"
+  type        = string
+}
+
+variable "cloudwatch_enable" {
+  description = "Enable AWS CloudWatch Agent for sending logs to AWS CloudWatch"
+  default     = true
+}
+
+variable "aws_ssm_enable" {
+  description = "Enable AWS Session Manager integration for easier SSH/RDP admin access to EC2 instances"
+  default     = true
+}
