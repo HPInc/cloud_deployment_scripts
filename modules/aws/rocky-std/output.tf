0cd7fe5468e554e2d7f7fc4a659db215d5c2ca9e
Visal Saosuo <visal.saosuo@hp.com>
2024-02-22 15:40:04 -0800

    AWS: Update copyright license on Rocky gfx and std
    This commit updates the copyright to "Copyright (c) 2024 HP
    Development Company, L.P." These changes only apply to files
    in the ./rocky-gfx and ./rocky-std directory.
    
    Signed-off-by: Visal Saosuo <visal.saosuo@hp.com>

--- modules/aws/rocky-std/output.tf
+++ modules/aws/rocky-std/output.tf
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Teradici Corporation
+ * © Copyright 2024 HP Development Company, L.P.
  * 
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
