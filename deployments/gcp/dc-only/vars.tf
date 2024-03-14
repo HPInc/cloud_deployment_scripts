56eccaa71ab266d4536403d344279340b8fe3097
Daniel Bergel <daniel.bergel@hp.com>
2024-03-14 12:03:31 +0000

    GCP: Updated OS images: CentOS 7, Rocky 8, Windows
    projects/centos-cloud/global/images/centos-7-v20240312
    projects/rocky-linux-cloud/global/images/rocky-linux-8-v20240312
    projects/windows-cloud/global/images/windows-server-2019-dc-v20240313
    
    Signed-off-by: Daniel Bergel <daniel.bergel@hp.com>

--- deployments/gcp/dc-only/vars.tf
+++ deployments/gcp/dc-only/vars.tf
@@ -87,7 +87,7 @@ variable "dc_disk_size_gb" {
 
 variable "dc_disk_image" {
   description = "Disk image for the Domain Controller"
-  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20240214"
+  default     = "projects/windows-cloud/global/images/windows-server-2019-dc-v20240313"
 }
 
 variable "dc_admin_password" {
